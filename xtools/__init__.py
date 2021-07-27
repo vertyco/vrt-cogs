51a7960e8b5eae8195faad17f077fe3b9f8e99be
vertyco <alex.c.goble@gmail.com>
2021-07-26 22:52:26 -0400

    end user statement

--- xtools/__init__.py
+++ xtools/__init__.py
@@ -1,6 +1,6 @@
 from .xtools import XTools
 ___red_end_user_data_statement__ = (
-    "This cog does not persistently store data about users."
+    "This cog uses the xbl.io API and does not persistently store data about users."
 )
 
 
