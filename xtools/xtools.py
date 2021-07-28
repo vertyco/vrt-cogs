72f83cdc0617e6b03c307db9910520f0282a752c
vertyco <alex.c.goble@gmail.com>
2021-07-28 00:18:26 -0400

    apikey bugfix and ctx.typing

--- xtools/xtools.py
+++ xtools/xtools.py
@@ -9,7 +9,7 @@ class XTools(commands.Cog):
     """
 
     __author__ = "Vertyco"
-    __version__ = "0.0.7"
+    __version__ = "0.0.9"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
