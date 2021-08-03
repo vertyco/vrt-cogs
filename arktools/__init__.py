77ff43e77ca678a497cbe45e7d362d22f46b8397
vertyco <alex.c.goble@gmail.com>
2021-08-02 22:31:24 -0400

    init update

--- arktools/__init__.py
+++ arktools/__init__.py
@@ -1,7 +1,7 @@
 from .arktools import ArkTools
 
 ___red_end_user_data_statement__ = (
-    "Thank you for installing! Use `[p]arktools` for more info.\n**Disclosure:** For large clusters with many servers, this cog may use considerable network traffic over time, especially with all features enabled."
+    "Thank you for installing! Use `[p]arktools` for more info.\n**Disclosure:** For large clusters with many servers, this cog may use considerable network I/O over time, especially with all features enabled.\nAlso remember to reload the cog when youre finished adding your servers."
 )
 
 
