04bc9ed7cc678a43d7a1286738b7164911980946
vertyco <alex.c.goble@gmail.com>
2021-08-05 23:03:17 -0400

    offline timer float rounding

--- arktools/arktools.py
+++ arktools/arktools.py
@@ -606,7 +606,7 @@ class ArkTools(commands.Cog):
                         if count >= 60:
                             count = count / 60
                             inc = "Hours."
-                            count = str(round(count, 2))
+                            count = str(round(count, 1))
 
                         status += f"{guild.get_channel(channel).mention}: Offline for {count} {inc}\n"
                         alertmsg = guild.get_channel(settings["clusters"][cluster]["adminlogchannel"])
