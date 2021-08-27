9667c3fd615932568b4acd3dba171d2f77e8672c
vertyco <alex.c.goble@gmail.com>
2021-08-26 22:54:50 -0400

    end user statement update for user playtime tracking

--- arktools/__init__.py
+++ arktools/__init__.py
@@ -1,7 +1,7 @@
 from .arktools import ArkTools
 
 ___red_end_user_data_statement__ = (
-    "Thank you for installing! Use `[p]arktools` for more info.\n**Disclosure:** For large clusters with many servers, this cog may use considerable network I/O over time, especially with all features enabled.\nAlso remember to reload the cog when youre finished adding your servers."
+    "Thank you for installing! Use `[p]arktools` for more info.\nBe sure to run the refresh command after adding/removing clusters and servers.\n**Disclosure:** This cog may use considerable network traffic over time depending on how many servers you have.\nThis cog stores Gamertag Names, time played per map, and API keys for Xbox self-host accounts."
 )
 
 
