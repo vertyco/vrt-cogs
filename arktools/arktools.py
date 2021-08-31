5a6d2b9bc3c05841aaec9b9554cccf11a2fa751d
vertyco <alex.c.goble@gmail.com>
2021-08-30 21:12:25 -0400

    bugfix

--- arktools/arktools.py
+++ arktools/arktools.py
@@ -393,7 +393,7 @@ class ArkTools(commands.Cog):
                                                 await reply.delete()
                                             except discord.NotFound:
                                                 pass
-                                            await msg.edit(embed=embed)
+                                            return await msg.edit(embed=embed)
                     if playerl == '':
                         embed = discord.Embed(description=f"No Gamertag set for **{ctx.author.name}**!\n"
                                                           f"Set a Gamertag with `{ctx.prefix}arktools register <Gamertag>`")
