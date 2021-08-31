08020c70bb8d199e0cd5cd153ec4ab437855fd05
vertyco <alex.c.goble@gmail.com>
2021-08-31 00:00:25 -0400

    info update

--- fluent/__init__.py
+++ fluent/__init__.py
@@ -1,7 +1,7 @@
 from .fluent import Fluent
 
 ___red_end_user_data_statement__ = (
-    "This cog does not persistently store data about users."
+    "This cog does not persistently store data about users. This cog uses google's free translator api"
 )
 
 
