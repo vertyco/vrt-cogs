6052a89e8d69e15378c2a8ce976a7aa10cb379e8
vertyco <alex.c.goble@gmail.com>
2021-09-08 19:57:18 -0400

    multi-server support

--- dayztools/dayztools.py
+++ dayztools/dayztools.py
@@ -70,86 +70,92 @@ class DayZTools(commands.Cog):
                 if service_data["status"] == "error":
                     return service_data
                 # Save service ID
-                service_id = service_data["data"]["services"][0]["id"]
-                info_req = f"https://api.nitrado.net/services/{service_id}:id/gameservers"
-                # Make another request to get game server info with id
-                async with self.session.get(info_req, headers=header) as info:
-                    info_data = await info.json()
-                    # save username, user id, ip, port, query_port, memory_mb, game_raw(dayzxb or dayzps), game_human,
-                    # last update, version, player_current, Player_max
-                    server = info_data["data"]["gameserver"]
-                    if "version" in server["query"]:
-                        version = server["query"]["version"]
-                    else:
-                        version = "Unknown"
-                    if "player_current" in server["query"]:
-                        players = server["query"]["player_current"]
-                    else:
-                        players = "None"
-                    if "player_max" in server["query"]:
-                        playermax = server["query"]["player_max"]
-                    else:
-                        playermax = "Unknown"
-                    self.servercache[guild_id] = {
-                        "user": server["username"],
-                        "user_id": server["user_id"],
-                        "ip": server["ip"],
-                        "port": server["port"],
-                        "query": server["query_port"],
-                        "memory": server["memory_mb"],
-                        "game_raw": server["game"],
-                        "game_name": server["game_human"],
-                        "status": server["status"],
-                        "location": server["location"],
-                        "version": version,
-                        "last_update": server["game_specific"]["last_update"],
-                        "players": players,
-                        "playermax": playermax,
-                        "service_id": server["service_id"],
-                        "ntoken": ntoken
-                    }
+                self.servercache[guild_id] = {}
+                self.playerlist[guild.id] = {}
+                self.killfeed[guild.id] = {}
+                for item in service_data["data"]["services"]:
+                    service_id = item["id"]
+                    info_req = f"https://api.nitrado.net/services/{service_id}:id/gameservers"
+                    # Make another request to get game server info with id
+                    async with self.session.get(info_req, headers=header) as info:
+                        info_data = await info.json()
+                        # save username, user id, ip, port, query_port, memory_mb,
+                        # game_raw(dayzxb or dayzps), game_human,
+                        # last update, version, player_current, Player_max
+                        server = info_data["data"]["gameserver"]
+                        if "version" in server["query"]:
+                            version = server["query"]["version"]
+                        else:
+                            version = "Unknown"
+                        if "player_current" in server["query"]:
+                            players = server["query"]["player_current"]
+                        else:
+                            players = "None"
+                        if "player_max" in server["query"]:
+                            playermax = server["query"]["player_max"]
+                        else:
+                            playermax = "Unknown"
+                        self.servercache[guild_id][service_id] = {
+                            "user": server["username"],
+                            "user_id": server["user_id"],
+                            "ip": server["ip"],
+                            "port": server["port"],
+                            "query": server["query_port"],
+                            "memory": server["memory_mb"],
+                            "game_raw": server["game"],
+                            "game_name": server["game_human"],
+                            "status": server["status"],
+                            "location": server["location"],
+                            "version": version,
+                            "last_update": server["game_specific"]["last_update"],
+                            "players": players,
+                            "playermax": playermax,
+                            "service_id": server["service_id"],
+                            "ntoken": ntoken
+                        }
             await self.server_status(guild)
             return service_data
 
     # Maintains an embed of server info
     async def server_status(self, guild):
-        server = self.servercache[guild.id]
-        memory = server["memory"]
-        game_name = server["game_name"]
-        version = server["version"]
-        last_update = server["last_update"]
-        if last_update is not None:
-            time = datetime.datetime.fromisoformat(last_update)
-            time = time.strftime('%m/%d/%Y at %H:%M:%S')
-        else:
-            time = "Unknown"
-        players = server["players"]
-        playermax = server["playermax"]
-        status = server["status"]
-        location = server["location"]
-
         channeldata = await self.config.guild(guild).statuschannel()
         messagedata = await self.config.guild(guild).statusmessage()
         if not channeldata:
             return
-
-        # Embed setup
         embed = discord.Embed(
             timestamp=datetime.datetime.utcnow(),
-            title=f"{game_name} Server Status",
+            title=f"Server Status",
             color=discord.Color.random(),
-            description=f"**Players:** `{players}/{playermax}`"
+            description=f"{guild.name}'s servers\n"
         )
-        embed.set_thumbnail(url=guild.icon_url)
-        embed.add_field(
-            name="Server Info",
-            value=f"Server Status: `{status}`\n"
-                  f"Current Version: `{version}`\n"
-                  f"Last Updated: `{time}`\n"
-                  f"Location: `{location}`\n"
-                  f"Memory: `{memory} MB`",
-            inline=False
-            )
+        for item in self.servercache[guild.id]:
+            server = self.servercache[guild.id][item]
+            memory = server["memory"]
+            game_name = server["game_name"]
+            version = server["version"]
+            last_update = server["last_update"]
+            if last_update is not None:
+                time = datetime.datetime.fromisoformat(last_update)
+                time = time.strftime('%m/%d/%Y at %H:%M:%S')
+            else:
+                time = "Unknown"
+            players = server["players"]
+            playermax = server["playermax"]
+            status = server["status"]
+            location = server["location"]
+
+            # Embed setup
+            embed.set_thumbnail(url=guild.icon_url)
+            embed.add_field(
+                name=f"{game_name} Info",
+                value=f"**Players:** `{players}/{playermax}`"
+                      f"Server Status: `{status}`\n"
+                      f"Current Version: `{version}`\n"
+                      f"Last Updated: `{time}`\n"
+                      f"Location: `{location}`\n"
+                      f"Memory: `{memory} MB`",
+                inline=False
+                )
         channel = guild.get_channel(channeldata)
 
         msgtoedit = None
@@ -179,26 +185,27 @@ class DayZTools(commands.Cog):
             guild = self.bot.get_guild(int(guild_id))
             settings = await self.config.guild(guild).all()
             if guild_id in self.servercache:
-                server = self.servercache[guild_id]
+                servers = self.servercache[guild_id]
             else:
                 continue
-            user = server["user"]
-            sid = server["service_id"]
-            ntoken = server["ntoken"]
-            header = {"Authorization": f"Bearer {ntoken}"}
-            game = server["game_raw"]
-            if game == "dayzxb":
-                logpath = "dayzxb/config/DayZServer_X1_x64.ADM"
-            else:
-                logpath = "dayzps/config/DayZServer_PS4_x64.ADM"
-            log_req = f"https://api.nitrado.net/services/{sid}:id/gameservers/file_server/download?file=/games/{user}/noftp/{logpath}"
-            async with self.session.get(log_req, headers=header) as geturl:
-                data = await geturl.json()
-                if "data" in data:
-                    url = data["data"]["token"]["url"]
-                    async with self.session.get(url, headers=header) as logs:
-                        logs_raw = await logs.text()
-                        await self.log_handler(logs_raw, guild, settings)
+            for server in servers:
+                user = servers[server]["user"]
+                sid = servers[server]["service_id"]
+                ntoken = servers[server]["ntoken"]
+                header = {"Authorization": f"Bearer {ntoken}"}
+                game = servers[server]["game_raw"]
+                if game == "dayzxb":
+                    logpath = "dayzxb/config/DayZServer_X1_x64.ADM"
+                else:
+                    logpath = "dayzps/config/DayZServer_PS4_x64.ADM"
+                log_req = f"https://api.nitrado.net/services/{sid}:id/gameservers/file_server/download?file=/games/{user}/noftp/{logpath}"
+                async with self.session.get(log_req, headers=header) as geturl:
+                    data = await geturl.json()
+                    if "data" in data:
+                        url = data["data"]["token"]["url"]
+                        async with self.session.get(url, headers=header) as logs:
+                            logs_raw = await logs.text()
+                            await self.log_handler(logs_raw, guild, settings, server)
 
     @server_logs.before_loop
     async def before_server_logs(self):
@@ -206,7 +213,7 @@ class DayZTools(commands.Cog):
         await asyncio.sleep(5)
 
     # Handles logs and sends to respective channel
-    async def log_handler(self, logs_raw, guild, settings):
+    async def log_handler(self, logs_raw, guild, settings, server):
         logs = logs_raw.split("\n")
         newkillfeed = ""
         newplayerlist = ""
@@ -215,15 +222,15 @@ class DayZTools(commands.Cog):
                 newplayerlist += f"{log}\n"
             if "(DEAD)" in log or "committed suicide" in log:
                 newkillfeed += f"{log}\n"
-        await self.playerlogging(newplayerlist, guild, settings)
-        await self.killfeeder(newkillfeed, guild, settings)
+        await self.playerlogging(newplayerlist, guild, settings, server)
+        await self.killfeeder(newkillfeed, guild, settings, server)
 
     # Handles killfeed
-    async def killfeeder(self, newkillfeed, guild, settings):
+    async def killfeeder(self, newkillfeed, guild, settings, server):
         klog = guild.get_channel(settings["killfeed"])
         klogs = newkillfeed.split("\n")
-        if guild.id not in self.killfeed:
-            self.killfeed[guild.id] = newkillfeed
+        if server not in self.killfeed[guild.id]:
+            self.killfeed[guild.id][server] = newkillfeed
             return
         else:
             for line in klogs:
@@ -233,7 +240,7 @@ class DayZTools(commands.Cog):
                 victim = str(re.search(r'"(.+?)"', line).group(1))
 
                 if "committed suicide" in line:
-                    k = self.checkfeed(guild, line)
+                    k = self.checkfeed(guild, line, server)
                     if k:
                         embed = discord.Embed(
                             title=f"💀 Suicide | {timestamp}",
@@ -243,7 +250,7 @@ class DayZTools(commands.Cog):
                         await klog.send(embed=embed)
                         await asyncio.sleep(2)
                 elif "hit by explosion" in line:
-                    k = self.checkfeed(guild, line)
+                    k = self.checkfeed(guild, line, server)
                     if k:
                         explosion = str(re.search(r'n \((.+?)\)$', line).group(1))
                         embed = discord.Embed(
@@ -254,7 +261,7 @@ class DayZTools(commands.Cog):
                         await klog.send(embed=embed)
                         await asyncio.sleep(2)
                 elif "killed by Player" in line:
-                    k = self.checkfeed(guild, line)
+                    k = self.checkfeed(guild, line, server)
                     if k:
                         killer = str(re.search(r'killed by Player "(.*?)"', line).group(1))
                         coords = str(re.search(r'pos=<(.*?)>', line).group(1))
@@ -274,7 +281,7 @@ class DayZTools(commands.Cog):
                         await klog.send(embed=embed)
                         await asyncio.sleep(2)
                 elif "bled out" in line:
-                    k = self.checkfeed(guild, line)
+                    k = self.checkfeed(guild, line, server)
                     if k:
                         embed = discord.Embed(
                             title=f"🩸 Bleed Out | {timestamp}",
@@ -284,7 +291,7 @@ class DayZTools(commands.Cog):
                         await klog.send(embed=embed)
                         await asyncio.sleep(2)
                 elif "Animal_CanisLupus_Grey" in line or "Animal_CanisLupus_White" in line:
-                    k = self.checkfeed(guild, line)
+                    k = self.checkfeed(guild, line, server)
                     if k:
                         embed = discord.Embed(
                             title=f"🐺 Wolf Kill | {timestamp}",
@@ -294,7 +301,7 @@ class DayZTools(commands.Cog):
                         await klog.send(embed=embed)
                         await asyncio.sleep(2)
                 elif "Animal_UrsusArctos" in line or "Brown Bear" in line:
-                    k = self.checkfeed(guild, line)
+                    k = self.checkfeed(guild, line, server)
                     if k:
                         embed = discord.Embed(
                             title=f"🐻 Beary Unfortunate | {timestamp}",
@@ -304,7 +311,7 @@ class DayZTools(commands.Cog):
                         await klog.send(embed=embed)
                         await asyncio.sleep(2)
                 elif "hit by FallDamage" in line:
-                    k = self.checkfeed(guild, line)
+                    k = self.checkfeed(guild, line, server)
                     if k:
                         embed = discord.Embed(
                             title=f"💀 Splattered | {timestamp}",
@@ -316,37 +323,37 @@ class DayZTools(commands.Cog):
                 else:
                     continue
 
-            self.killfeed[guild.id] = newkillfeed
+            self.killfeed[guild.id][server] = newkillfeed
 
-    def checkfeed(self, guild, line):
-        if line not in self.killfeed[guild.id]:
+    def checkfeed(self, guild, line, server):
+        if line not in self.killfeed[guild.id][server]:
             return line
 
     # Handles player join/leaves
-    async def playerlogging(self, newplayerlist, guild, settings):
+    async def playerlogging(self, newplayerlist, guild, settings, server):
         plog = guild.get_channel(settings["playerlog"])
         regex = r'(..:..:..).+"(.+)".+(\bconnected|disconnected)'
         newplayerlist = re.findall(regex, newplayerlist)
-        if guild.id not in self.playerlist:
-            self.playerlist[guild.id] = newplayerlist
+        if server not in self.playerlist[guild.id]:
+            self.playerlist[guild.id][server] = newplayerlist
             return
         else:
             for player in newplayerlist:
                 if player[2] == "connected":
-                    p = self.checkplayers(guild, player)
+                    p = self.checkplayers(guild, player, server)
                     if p:
                         await plog.send(f":green_circle: `{p[1]}` has connected to the server. - `{p[0]}`")
                         await asyncio.sleep(2)
 
                 elif player[2] == "disconnected":
-                    p = self.checkplayers(guild, player)
+                    p = self.checkplayers(guild, player, server)
                     if p:
                         await plog.send(f":red_circle: `{p[1]}` has left the server. - `{p[0]}`")
                         await asyncio.sleep(2)
 
-            self.playerlist[guild.id] = newplayerlist
+            self.playerlist[guild.id][server] = newplayerlist
 
-    def checkplayers(self, guild, player):
+    def checkplayers(self, guild, player, server):
         if player not in self.playerlist[guild.id]:
             return player
 
@@ -494,16 +501,16 @@ class DayZTools(commands.Cog):
             if not ntoken:
                 return await ctx.send("No API token has been set!")
             else:
-                server = self.servercache[ctx.guild.id]
-                sid = server["service_id"]
-                restart = f"https://api.nitrado.net/services/{sid}:id/gameservers/restart"
-                header = {"Authorization": f"Bearer {ntoken}"}
-                res = await self.apipost(restart, header)
-                status = res["status"]
-                if status == "success":
-                    return await ctx.send("Server reboot initiated...")
-                else:
-                    return await ctx.send("Server failed to reboot...")
+                for server in self.servercache[ctx.guild.id]:
+                    sid = server["service_id"]
+                    restart = f"https://api.nitrado.net/services/{sid}:id/gameservers/restart"
+                    header = {"Authorization": f"Bearer {ntoken}"}
+                    res = await self.apipost(restart, header)
+                    status = res["status"]
+                    if status == "success":
+                        return await ctx.send(f"Server reboot initiated for id: {server}...")
+                    else:
+                        return await ctx.send(f"Server failed to reboot for id: {server}...")
 
     @dayz_tools.command()
     @commands.admin()
@@ -515,13 +522,13 @@ class DayZTools(commands.Cog):
             if not ntoken:
                 return await ctx.send("No API token has been set!")
             else:
-                server = self.servercache[ctx.guild.id]
-                sid = server["service_id"]
-                stop = f"https://api.nitrado.net/services/{sid}:id/gameservers/stop"
-                header = {"Authorization": f"Bearer {ntoken}"}
-                res = await self.apipost(stop, header)
-                status = res["status"]
-                if status == "success":
-                    return await ctx.send("Server stopped...")
-                else:
-                    return await ctx.send("Server failed to stop...")
+                for server in self.servercache[ctx.guild.id]:
+                    sid = server["service_id"]
+                    stop = f"https://api.nitrado.net/services/{sid}:id/gameservers/stop"
+                    header = {"Authorization": f"Bearer {ntoken}"}
+                    res = await self.apipost(stop, header)
+                    status = res["status"]
+                    if status == "success":
+                        return await ctx.send(f"Server stopped for id: {server}...")
+                    else:
+                        return await ctx.send(f"Server failed to stop for id: {server}...")
