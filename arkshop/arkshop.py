8481c155a2dcf41fe7f77cd7721ab434a27884b6
vertyco <alex.c.goble@gmail.com>
2021-09-13 18:22:10 -0400

    shop view

--- arkshop/arkshop.py
+++ arkshop/arkshop.py
@@ -1298,13 +1298,13 @@ class ArkShop(commands.Cog):
             for item in shops[category]:
                 if shops[category][item]["options"] == {}:
                     price = shops[category][item]["price"]
-                    category_items += f"• {item}: {price}\n"
+                    category_items += f"ℹ {item}: {price}\n"
                 else:
-                    category_items += f"• {item}\n"
+                    category_items += f"ℹ {item}\n"
                     for k, v in shops[category][item]["options"].items():
                         price = v
                         option = k
-                        category_items += f"  -{option}: {price}\n"
+                        category_items += f"🅾 {option}: {price}\n"
 
         if ctx:
             for p in pagify(category_items):
