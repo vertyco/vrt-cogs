addf2ddec8f29e229b432aa2f9cf47aa0eda3726
vertyco <alex.c.goble@gmail.com>
2021-09-14 23:42:21 -0400

    leaderboard

--- arkshop/arkshop.py
+++ arkshop/arkshop.py
@@ -752,7 +752,32 @@ class ArkShop(commands.Cog):
     @commands.command(name="shoplb")
     async def shop_leaderboard(self, ctx):
         """Get leaderboard for most items bought"""
-        pass
+        logs = await self.config.guild(ctx.guild).logs()
+
+        shop_logs = {}
+        for item in logs["items"]:
+            count = logs["items"][item]["count"]
+            shop_logs[item] = count
+
+        sorted_items = sorted(shop_logs.items(), key=lambda x: x[1], reverse=True)
+        pages = math.ceil(len(sorted_items) / 10)
+        embeds = []
+        start = 0
+        stop = 10
+        for page in range(int(pages)):
+            if stop > len(sorted_items):
+                stop = len(sorted_items)
+            items = ""
+            for i in range(start, stop, 1):
+                name = sorted_items[i][0]
+                purchases = sorted_items[i][1]
+                items += f"**{name}**: `{purchases} purchases`\n"
+            embed = discord.Embed(
+                title="Shop Leaderboard",
+                description=items
+            )
+            embeds.append(embed)
+        return await self.paginate(ctx, embeds)
 
     @commands.command(name="shopstats")
     async def shop_stats(self, ctx):
