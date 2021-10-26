6a709ce26b93c30ad6fd884c3f47a6599c4ae1e0
vertyco <alex.c.goble@gmail.com>
2021-10-26 15:14:23 -0400

    [ArkTools] graph tweaks

--- arktools/arktools.py
+++ arktools/arktools.py
@@ -13,8 +13,6 @@ from rcon import Client
 
 import pandas as pd
 
-import plotly.express as px
-
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
@@ -582,33 +580,6 @@ class ArkTools(commands.Cog):
     async def init_config(self, ctx):
         await self.initialize()
         await ctx.tick()
-
-    # Keeping here for reference
-    @commands.command(name="plotly", hidden=True, disabled=True)
-    async def plotly_graph(self, ctx: commands.Context):
-        """Tried plotly version of playtime graph"""
-        settings = await self.config.guild(ctx.guild).all()
-        x = []
-        y = []
-        times = settings["serverstats"]["dates"]
-        counts = settings["serverstats"]["counts"]
-        for i in range(len(times) - 1):
-            timestamp = datetime.datetime.fromisoformat(times[i])
-            x.append(timestamp)
-            y.append(counts[i])
-        x.reverse()
-        y.reverse()
-        df = pd.DataFrame(
-            {
-                "time": x,
-                "players": y
-            }
-        )
-        fig = px.line(df, x='time', y='players')
-        fig.write_image("plot.png")
-        with open("plot.png", "rb") as file:
-            file = discord.File(file, "plot.png")
-            await ctx.send(file=file)
 
     # Thanks to Vexed#3211 for help with the Matplotlib logic :)
     @commands.command(name="servergraph", hidden=False)
