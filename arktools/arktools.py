ebfe8d6781ec2dd72f170b846baafff4e3950563
vertyco <alex.c.goble@gmail.com>
2021-12-21 23:08:18 -0500

    [ArkTools] - 2.5.28 test

--- arktools/arktools.py
+++ arktools/arktools.py
@@ -3373,7 +3373,9 @@ class ArkTools(commands.Cog):
                     pages += 1
                 new_message_list = []
                 count = 1
+                log.info(f"{pages} pages")
                 for p in strings:
+                    log.info(p)
                     if count == pages:
                         embed = discord.Embed(
                             description=p,
@@ -3386,17 +3388,21 @@ class ArkTools(commands.Cog):
                             color=discord.Color.random()
                         )
                     if count == 1:
+                        log.info(f"adding author")
                         embed.set_author(name="Server Status", icon_url=guild.icon_url)
                         embed.set_thumbnail(url=thumbnail)
                     if count == pages:
+                        log.info(f"setting image and sending")
                         embed.set_image(url=img)
                         message = await dest_channel.send(embed=embed, file=file)
                     else:
+                        log.info(f"trying to send the shit")
                         message = await dest_channel.send(embed=embed)
                     count += 1
                     new_message_list.append(message.id)
-
+                log.info(f"trying to cleanup old messages")
                 await self.status_cleaner(settings["status"], dest_channel)
+                log.info(f"set the fucking list")
                 await self.config.guild(guild).status.multi.set(new_message_list)
 
     @status_channel.before_loop
