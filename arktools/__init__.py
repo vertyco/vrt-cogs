ffd2b7797cc57720da353a6352201aab98defb2c
vertyco <alex.c.goble@gmail.com>
2021-12-28 08:52:39 -0500

    [ArkTools] - v2.7.34 update info.json

--- arktools/__init__.py
+++ arktools/__init__.py
@@ -1,7 +1,7 @@
 from .arktools import ArkTools
 
 ___red_end_user_data_statement__ = (
-    "Thank you for installing! Use `[p]arktools` and `[p]help ArkTools` for more info.\n**Disclosure:** This cog may use considerable network traffic over time depending on how many servers you have.\nThis cog stores Gamertag Names, time played per map, and optional token data for Xbox self-host accounts."
+    "This is probably one of the more I/O heavy cogs available and is NOT intended to be run on extremely small devices/VMs due to the sheer amount of socket connections, API calls, and task loops it utilizes.\nThis cog stores Gamertag Names, XUIDs, Discord IDs, time played per map, and optional token data for Xbox self-host accounts."
 )
 
 
