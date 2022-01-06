ea02a391af7ba03d88f67df32dca15d4387b1f9e
vertyco <alex.c.goble@gmail.com>
2022-01-05 21:01:56 -0500

    [Inspire] - cleanup old stuff

--- inspire/inspire.py
+++ inspire/inspire.py
@@ -1,15 +1,15 @@
-from redbot.core import commands, checks
-import aiohttp
-import json
 import random
+
+import aiohttp
 import discord
+from redbot.core import commands
 
 
 class Inspire(commands.Cog):
     """Get Inspiring Messages"""
 
     __author__ = "Vertyco"
-    __version__ = "0.0.1"
+    __version__ = "1.0.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
@@ -61,12 +61,6 @@ class Inspire(commands.Cog):
             "Sending you good thoughts—and hoping you believe in yourself just as much as I believe in you.",
             "I know you can do this!"
         ]
-
-        # for word in sad_words:
-        #     if word in message.content.lower():
-        #         return await message.channel.send(random.choice(starter_encouragements))
-        # else:
-        #     return
 
         if any(w in msg for w in sad_words):
             await message.channel.send(random.choice(starter_encouragements))
