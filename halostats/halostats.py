ab72745a2ef52c16178737cb3a757a0299a822d8
vertyco <alex.c.goble@gmail.com>
2022-01-17 22:22:56 -0500

    HaloStats - edit

--- halostats/halostats.py
+++ halostats/halostats.py
@@ -69,9 +69,8 @@ class HaloStats(commands.Cog):
             embed.set_thumbnail(url=LOADING)
             msg = await ctx.send(embed=embed)
             pages = await self.run_scraper(gamertag)
-            await msg.delete()
             if pages:
-                await menu(ctx, pages, DEFAULT_CONTROLS)
+                await menu(ctx, pages, DEFAULT_CONTROLS, msg)
             else:
                 await ctx.send(f"Couldnt find stats for {gamertag}")
 
