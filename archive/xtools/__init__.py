b2bd6ecbb946cf2be8ce19be608d9066a92fab60
vertyco <alex.c.goble@gmail.com>
2022-04-04 18:47:44 -0400

    cleanup

--- archive/xtools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .xtools import XTools
-___red_end_user_data_statement__ = (
-    "This cog does not persistently store data about users."
-)
-
-
-def setup(bot):
-    cog = XTools(bot)
-    bot.add_cog(cog)
--- tests/winapp_test.py
+++
@@ -1,9 +0,0 @@
-
-import unittest
-from appium import webdriver
-
-desired_caps = {}
-desired_caps["app"] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
-desired_caps["platformName"] = "Windows"
-desired_caps["deviceName"] = "WindowsPC"
-driver = webdriver.Remote("http://127.0.0.1:4723/", desired_caps)
