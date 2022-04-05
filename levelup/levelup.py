aeefcdd993ea3cc8c4ba2b690dbdd5c7e53ce709
vertyco <alex.c.goble@gmail.com>
2022-04-04 22:00:43 -0400

    sync

--- levelup/levelup.py
+++ levelup/levelup.py
@@ -47,7 +47,7 @@ LOADING = "https://i.imgur.com/l3p6EMX.gif"
 class LevelUp(commands.Cog):
     """Local Discord Leveling System"""
     __author__ = "Vertyco#0117"
-    __version__ = "1.1.4"
+    __version__ = "1.1.5"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
