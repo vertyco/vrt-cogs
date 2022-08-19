14a2718ca17eb49ba58d0817e6e97fc663690c4e
Alex <alex.c.goble@gmail.com>
2022-08-19 19:30:31 -0400

    meow - make setup function async right meow

--- meow/__init__.py
+++ meow/__init__.py
@@ -9,7 +9,7 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot):
+async def setup(bot):
     cog = Meow(bot)
     if discord.__version__ > "1.7.3":
         await bot.add_cog(cog)
