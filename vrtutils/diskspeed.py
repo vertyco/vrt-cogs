0f0a3fdf8facf948160709f94d8561c607701c87
Alex <alex.c.goble@gmail.com>
2022-08-22 13:40:26 -0400

    vrtutils - conversion logic for mb

--- vrtutils/diskspeed.py
+++ vrtutils/diskspeed.py
@@ -19,16 +19,16 @@ def get_write_speed(path, blocks_count=128, block_size=1048576):
     f = os.open(path, os.O_CREAT | os.O_WRONLY, 0o777)  # Low Level I/O
     w_times = []
     for i in range(blocks_count):
-        sys.stdout.flush()
         buff = os.urandom(block_size)
         start = time()
         os.write(f, buff)
         os.fsync(f)
         w_times.append(time() - start)
     os.close(f)
-
-    write_speed = blocks_count / sum(w_times)  # MB/s
-    return write_speed
+    one_mb = 1048576
+    ratio = (block_size / one_mb) if block_size >= one_mb else (one_mb / block_size)
+    write_speed = blocks_count / (sum(w_times) * ratio)
+    return write_speed  # MB/s
 
 
 def get_read_speed(path, blocks_count=128, block_size=1048576):
@@ -47,9 +47,10 @@ def get_read_speed(path, blocks_count=128, block_size=1048576):
             break  # If EOF Reached
         r_times.append(t)
     os.close(f)
-
-    read_speed = blocks_count / sum(r_times)  # MB/s
-    return read_speed
+    one_mb = 1048576
+    ratio = (block_size / one_mb) if block_size >= one_mb else (one_mb / block_size)
+    read_speed = blocks_count / (sum(r_times) * ratio)
+    return read_speed  # MB/s
 
 
 def get_disk_speed(path, blocks_count, block_size):
@@ -67,7 +68,6 @@ def get_disk_speed(path, blocks_count, block_size):
 if __name__ == "__main__":
     maindir = os.getcwd()
     pathname = os.path.join(maindir, "IOTest")
-    one_mb = 1048576
     print(get_write_speed(pathname))
     print(get_read_speed(pathname))
     os.remove(pathname)
