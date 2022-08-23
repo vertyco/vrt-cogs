04b8170b6e8104f42b9ffc6f9cae46875af26fcb
Alex <alex.c.goble@gmail.com>
2022-08-22 20:53:39 -0400

    levelup - bundled_data_path got deleted somehow

--- levelup/generator.py
+++ levelup/generator.py
@@ -8,6 +8,7 @@ import colorgram
 import requests
 from PIL import Image, ImageDraw, ImageFont
 from redbot.core.i18n import Translator
+from redbot.core.data_manager import bundled_data_path
 
 log = logging.getLogger("red.vrt.levelup.generator")
 _ = Translator("LevelUp", __file__)
