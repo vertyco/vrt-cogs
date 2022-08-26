efec561536082282a618361af934ea2104e6a75c
Alex <alex.c.goble@gmail.com>
2022-08-25 22:11:00 -0400

    youtubedownloader - initial commit

---
+++ youtubedownloader/__init__.py
@@ -0,0 +1,17 @@
+import json
+from pathlib import Path
+
+import discord
+
+from .base import YouTubeDownloader
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+async def setup(bot):
+    cog = YouTubeDownloader(bot)
+    if discord.__version__ > "1.7.3":
+        await bot.add_cog(cog)
+    else:
+        bot.add_cog(cog)
