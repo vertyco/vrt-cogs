17125973c4ce726b713d9c4d2521725c18cd807a
Alex <alex.c.goble@gmail.com>
2022-09-02 20:42:02 -0400

    PUPPER *and other tweaks*

--- pupper/__init__.py
+++ pupper/__init__.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 import discord
 
-from .levelup import LevelUp
+from .pupper import Pupper
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot):
-    cog = LevelUp(bot)
+    cog = Pupper(bot)
     if discord.__version__ > "1.7.3":
         await bot.add_cog(cog)
     else:
