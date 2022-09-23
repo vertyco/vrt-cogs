42c105d9770e796b8dd7e967f59ce2e036c23216
Alex <alex.c.goble@gmail.com>
2022-09-23 16:47:17 -0400

    levelup - color randomizing

--- levelup/generator.py
+++ levelup/generator.py
@@ -94,17 +94,17 @@ class Generator:
         namebox = (bar_start, name_y, bar_start + 50, name_y + 50)
         namesection = self.get_sample_section(card, namebox)
         namebg = self.get_img_color(namesection)
-        while self.distance(namecolor, namebg) < 200:
+        while self.distance(namecolor, namebg) < 220:
             namecolor = self.rand_rgb()
         statbox = (bar_start, stats_y, bar_start + 400, bar_top)
         statsection = self.get_sample_section(card, statbox)
         statbg = self.get_img_color(statsection)
-        while self.distance(statcolor, statbg) < 200:
+        while self.distance(statcolor, statbg) < 220:
             statcolor = self.rand_rgb()
         lvlbox = (bar_start, bar_top, bar_end, bar_bottom)
         barsection = self.get_sample_section(card, lvlbox)
         barbg = self.get_img_color(barsection)
-        while self.distance(lvlbarcolor, barbg) < 200:
+        while self.distance(lvlbarcolor, barbg) < 220:
             lvlbarcolor = self.rand_rgb()
 
         # get profile pic
