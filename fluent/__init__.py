ebc2df0d541557660f1e809aaa8676d6c7e27c9c
Alex <alex.c.goble@gmail.com>
2022-12-11 19:18:15 -0500

    Fluent - Flowery translate API addition

--- fluent/__init__.py
+++ fluent/__init__.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 import discord
+from redbot.core.bot import Red
 
 from .fluent import Fluent
 
@@ -9,7 +10,7 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-async def setup(bot):
+async def setup(bot: Red):
     cog = Fluent(bot)
     if discord.__version__ > "1.7.3":
         await bot.add_cog(cog)
---
+++ fluent/api.py
@@ -0,0 +1,67 @@
+import functools
+from concurrent.futures import ThreadPoolExecutor
+from typing import Optional
+
+import googletrans
+from aiohttp import ClientSession, ClientTimeout, ClientResponseError, ClientConnectorError
+from redbot.core.bot import Red
+
+
+class Result:
+    def __init__(self, text: str, src: str, dest: str):
+        self.text = text
+        self.src = src
+        self.dest = dest
+
+    def __str__(self):
+        return f"Result: {self.text}, source: {self.src}, target: {self.dest}"
+
+
+class TranslateManager:
+    def __init__(self, bot: Red):
+        self.bot = bot
+        self.threadpool = ThreadPoolExecutor(max_workers=1, thread_name_prefix="fluent")
+
+    async def translate(self, text: str, target_lang: str) -> Optional[Result]:
+        res = await self.google(text, target_lang)
+        if res is None or res.text == text:
+            res = await self.flowery(text, target_lang)
+        return res
+
+    async def google(self, text: str, target_lang: str) -> Optional[Result]:
+        translator = googletrans.Translator()
+        try:
+            res = await self.bot.loop.run_in_executor(
+                self.threadpool,
+                functools.partial(translator.translate, text, target_lang)
+            )
+            result = Result(
+                text=res.text,
+                src=res.src,
+                dest=res.dest
+            )
+            return result
+        except AttributeError:
+            return None
+
+    @staticmethod
+    async def flowery(text: str, target_lang: str) -> Optional[Result]:
+        endpoint = "https://api.flowery.pw/v1/translation/translate"
+        params = {
+            "text": text,
+            "result_language_code": target_lang
+        }
+        timeout = ClientTimeout(total=6)
+        try:
+            async with ClientSession(timeout=timeout) as session:
+                async with session.get(url=endpoint, params=params) as res:
+                    if res.status == 200:
+                        data = await res.json()
+                        result = Result(
+                            text=data["text"],
+                            src=data["language"]["original"],
+                            dest=data["language"]["result"]
+                        )
+                        return result
+        except (ClientResponseError, ClientConnectorError):
+            return None
