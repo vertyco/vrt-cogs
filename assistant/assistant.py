d6bc39119b3f298430df6b2b73e45f593d06e670
Vert <alex.c.goble@gmail.com>
2023-04-23 12:49:40 -0400

    Assistant - hotfix

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -27,7 +27,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "1.0.5"
+    __version__ = "1.0.6"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
