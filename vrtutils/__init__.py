b4039ddd72ad05c0fa7a3b7d372d7588629de4c6
Vert <alex.c.goble@gmail.com>
2023-05-08 11:56:24 -0400

    dpy2 only for some cogs

--- vrtutils/__init__.py
+++ vrtutils/__init__.py
@@ -1,5 +1,3 @@
-
-import discord
 from redbot.core.utils import get_end_user_data_statement
 
 from .vrtutils import VrtUtils
@@ -9,7 +7,4 @@ __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot):
     cog = VrtUtils(bot)
-    if discord.__version__ > "1.7.3":
-        await bot.add_cog(cog)
-    else:
-        bot.add_cog(cog)
+    await bot.add_cog(cog)
