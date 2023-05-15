062ffd7f161d4ea15b126827a5ff10f4022c090e
Vert <alex.c.goble@gmail.com>
2023-05-14 20:51:17 -0400

    Assistant - bugfix

--- assistant/api.py
+++ assistant/api.py
@@ -59,7 +59,7 @@ class API(MixinMeta):
         system_prompt = conf.system_prompt.format(**params)
         initial_prompt = conf.prompt.format(**params)
 
-        query_embedding = get_embedding(message, engine="text-embedding-ada-002")
+        query_embedding = get_embedding(text=message, key=conf.api_key)
         embeddings = conf.get_related_embeddings(query_embedding)
         if embeddings:
             initial_prompt += "\nContext:\n"
