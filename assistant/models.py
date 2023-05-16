05721ba339c8307286251f994de70256166529e8
Vert <alex.c.goble@gmail.com>
2023-05-16 10:42:34 -0400

    Assistant - read uploaded files

--- assistant/models.py
+++ assistant/models.py
@@ -9,6 +9,7 @@ from pydantic import BaseModel
 from .common.utils import num_tokens_from_string
 
 MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4-32k"]
+READ_EXTENSIONS = [".txt", ".py", ".json", ".xml", ".html", ".ini", ".css"]
 
 
 class Embedding(BaseModel):
