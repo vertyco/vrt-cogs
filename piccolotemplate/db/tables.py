832857211fe3e27de96cb1263805aca020e6c5ff
Vert <alex.c.goble@gmail.com>
2023-06-02 09:01:51 -0400

    remove template

--- piccolotemplate/db/tables.py
+++
@@ -1,7 +0,0 @@
-from piccolo.columns import Serial, Text
-from piccolo.table import Table
-
-
-class MyTable(Table):
-    id = Serial
-    name = Text(default="Example", help_text="This would show up in piccolo admin")
