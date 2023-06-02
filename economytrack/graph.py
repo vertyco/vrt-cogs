9b8610a3c374187ebd033d528bf8985d2b6f6c94
Vert <alex.c.goble@gmail.com>
2023-06-02 16:52:01 -0400

    EconomyTrack - cleanup

--- economytrack/graph.py
+++ economytrack/graph.py
@@ -4,14 +4,10 @@ from io import BytesIO
 import discord
 import pandas as pd
 from plotly import express as px
-from redbot.core.i18n import Translator, cog_i18n
 
 from economytrack.abc import MixinMeta
 
-_ = Translator("EconomyTrackCommands", __file__)
 
-
-@cog_i18n(_)
 class PlotGraph(MixinMeta):
     async def get_plot(self, df: pd.DataFrame, y_label: str) -> discord.File:
         return await asyncio.to_thread(self.make_plot, df, y_label)
@@ -21,9 +17,10 @@ class PlotGraph(MixinMeta):
         fig = px.line(
             df,
             template="plotly_dark",
-            labels={"ts": _("Date"), "value": y_label},
+            labels={"ts": "Date", "value": y_label},
         )
         fig.update_xaxes(tickformat="%I:%M %p\n%b %d %Y")
+        fig.update_yaxes(tickformat=".1f")
         fig.update_layout(
             showlegend=False,
         )
