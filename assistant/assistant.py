95ddcb29af05eeba556f6c05c4350d70feed45d7
Vert <alex.c.goble@gmail.com>
2023-06-04 15:21:07 -0400

    Assistant - reply optimization

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -29,7 +29,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "2.5.0"
+    __version__ = "2.5.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
