3dece182eced4b8a9a0ec1ed75607e6ab39aa3e1
Vert <alex.c.goble@gmail.com>
2023-06-05 13:39:28 -0400

    more extensions

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -30,7 +30,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "2.5.5"
+    __version__ = "2.5.6"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
