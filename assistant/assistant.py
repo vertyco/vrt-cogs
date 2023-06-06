0fb4f960fdddbfda2b74a7f18a3749320a988256
Vert <alex.c.goble@gmail.com>
2023-06-06 15:54:40 -0400

    Assistant - recall last message

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -30,7 +30,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "2.6.0"
+    __version__ = "2.6.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
