f584ae831f06922e5fa3a5dc701e18ad015ebdd7
Vert <alex.c.goble@gmail.com>
2023-06-08 15:52:13 -0400

    Assistant - handle users deleting their questions immediately

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -30,7 +30,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "2.6.3"
+    __version__ = "2.6.4"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
