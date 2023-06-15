3451be003b0751616df97c6244b4fc2a38dfa5db
Vert <alex.c.goble@gmail.com>
2023-06-15 09:20:14 -0400

    Assistant - handle longer code

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -30,7 +30,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "2.9.3"
+    __version__ = "2.9.4"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
