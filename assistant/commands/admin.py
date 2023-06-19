988abf56377eb2b4a2deb3c4638fbc6a9552dbc1
Vert <alex.c.goble@gmail.com>
2023-06-19 10:47:21 -0400

    Assistant - desc

--- assistant/commands/admin.py
+++ assistant/commands/admin.py
@@ -484,6 +484,7 @@ class Admin(MixinMeta):
         - gpt-3.5-turbo-0613
         - gpt-3.5-turbo-16k-0613
         - gpt-4-0613
+        - gpt-4-32k-0613
         """
         conf = self.db.get_conf(ctx.guild)
         if conf.use_function_calls:
