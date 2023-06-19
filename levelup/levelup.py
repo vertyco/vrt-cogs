8e6565a45ed5315e8af458d8a316bc2776664155
Vert <alex.c.goble@gmail.com>
2023-06-19 16:02:49 -0400

    LevelUp - fix level progress bar

--- levelup/levelup.py
+++ levelup/levelup.py
@@ -70,7 +70,7 @@ class LevelUp(UserCommands, Generator, commands.Cog, metaclass=CompositeMetaClas
     """Your friendly neighborhood leveling system"""
 
     __author__ = "Vertyco#0117"
-    __version__ = "3.0.1"
+    __version__ = "3.0.2"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
