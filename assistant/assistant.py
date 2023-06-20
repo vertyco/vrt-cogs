ee49b05f1601f79e7e6fd7f89e38da3435f75d42
Vert <alex.c.goble@gmail.com>
2023-06-20 13:04:26 -0400

    Assistant - sway the model away from trying the same invalid function

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -32,7 +32,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "3.2.5"
+    __version__ = "3.2.6"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
