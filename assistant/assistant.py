e02ee4f0c6fe0c7a96b7d8a9610715f7260e99e5
Vert <alex.c.goble@gmail.com>
2023-06-21 21:20:29 -0400

    Assistant - iterative decay tweak

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -44,7 +44,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "3.4.13"
+    __version__ = "3.4.14"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
