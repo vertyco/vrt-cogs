4450236ffae4e08abc01a93b068a4a489b012ffd
Vert <alex.c.goble@gmail.com>
2023-06-29 00:09:38 -0400

    Assistant - local model return string fix

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -54,7 +54,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "4.0.4"
+    __version__ = "4.0.5"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
