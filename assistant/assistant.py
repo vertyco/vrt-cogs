308e97e0842528e25a07aa14e10921fb2f2be9b6
Vert <alex.c.goble@gmail.com>
2023-07-01 14:18:49 -0400

    Assistant - handle local model failing

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -53,7 +53,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "4.2.5"
+    __version__ = "4.2.6"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
