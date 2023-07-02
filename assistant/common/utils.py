dd33f3d88ce40dbcd336a598ace2ce2bb088f720
Vert <alex.c.goble@gmail.com>
2023-07-02 17:56:21 -0400

    cleaner prompting

--- assistant/common/utils.py
+++ assistant/common/utils.py
@@ -115,54 +115,36 @@ def json_schema_invalid(schema: dict) -> str:
     return missing
 
 
-def compile_messages(
-    messages: List[dict], default_prompt_header=True, default_prompt_footer=True
-) -> str:
-    # Yoinked from https://github.com/nomic-ai/gpt4all/blob/main/gpt4all-bindings/python/gpt4all/gpt4all.py#L260
-    """
-    Helper method for building a prompt using template from list of messages.
-
-    Args:
-        messages:  List of dictionaries. Each dictionary should have a "role" key
-            with value of "system", "assistant", or "user" and a "content" key with a
-            string value. Messages are organized such that "system" messages are at top of prompt,
-            and "user" and "assistant" messages are displayed in order. Assistant messages get formatted as
-            "Response: {content}".
-        default_prompt_header: If True (default), add default prompt header after any system role messages and
-            before user/assistant role messages.
-        default_prompt_footer: If True (default), add default footer at end of prompt.
-
-    Returns:
-        Formatted prompt.
-    """
-    full_prompt = ""
+def compile_messages(messages: List[dict]) -> str:
+    system = ""
+    context = ""
+    prompt = ""
+    for message in messages:
+        if message["role"] == "system":
+            system += message["content"] + "\n"
 
     for message in messages:
         if message["role"] == "system":
-            system_message = message["content"] + "\n"
-            full_prompt += system_message
-
-    if default_prompt_header:
-        full_prompt += """### Instruction:
-        The prompt below is a question to answer, a task to complete, or a conversation
-        to respond to; decide which and write an appropriate response.
-        \n### Prompt: """
-
-    for message in messages:
+            continue
         content = message["content"].strip()
-        if message["role"] == "user":
-            if content.startswith("Context"):
-                full_prompt += f"\n{content}"
-            else:
-                full_prompt += f"\n### Human: \n{content}"
-        if message["role"] == "assistant":
-            assistant_message = "\n### Response: " + content
-            full_prompt += assistant_message
+        if "### Context:" in content:
+            context += f"{content.replace('### Context:', '').strip()}\n"
+        elif message["role"] == "user":
+            prompt += f"### Prompt: {content}\n"
+        elif message["role"] == "assistant":
+            prompt += f"### Response: {content}\n"
 
-    if default_prompt_footer:
-        full_prompt += "\n### Response:"
+    final = ""
+    if system:
+        final += f"### Instruction: {system}\n"
+    if context:
+        final += f"### Context: {context}\n"
+    if prompt:
+        final += prompt
 
-    return full_prompt
+    final += "\n### Response:"
+
+    return final.strip()
 
 
 def get_params(
