58b2bdf05cd9fc55a798aaee0705678e01cbbcc2
Vert <alex.c.goble@gmail.com>
2023-07-16 12:41:24 -0400

    Tickets - stop modal view

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -30,7 +30,7 @@ class Tickets(TicketCommands, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "Vertyco"
-    __version__ = "2.0.5"
+    __version__ = "2.0.6"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
