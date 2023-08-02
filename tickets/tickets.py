9b1607459192c98cfa3753a20884aff1b6e15742
Vert <alex.c.goble@gmail.com>
2023-08-02 14:03:13 -0400

    Tickets - alt message for improperly setup permissions

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -30,7 +30,7 @@ class Tickets(TicketCommands, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "Vertyco"
-    __version__ = "2.0.8"
+    __version__ = "2.0.9"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
