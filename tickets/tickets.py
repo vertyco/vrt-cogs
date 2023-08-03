aa60ac777fc4a566a0b61db7af228c8288802f88
Vert <alex.c.goble@gmail.com>
2023-08-03 11:36:23 -0400

    Tickets - fix ticket message cancel

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -30,7 +30,7 @@ class Tickets(TicketCommands, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "Vertyco"
-    __version__ = "2.0.10"
+    __version__ = "2.0.11"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
