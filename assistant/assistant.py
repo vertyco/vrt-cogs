2e186044fe86492b2fca2d3dfb7c04c9d40d58de
Vert <alex.c.goble@gmail.com>
2023-08-06 19:39:46 -0400

    Assistant - enforce max length on embed modal

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -51,7 +51,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "4.11.11"
+    __version__ = "4.11.12"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
