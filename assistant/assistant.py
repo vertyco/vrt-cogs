9d446814a80e4e426ee12c91b62b8dc29b45e89d
Vert <alex.c.goble@gmail.com>
2023-08-06 11:35:15 -0400

    Assistant - chathelp command

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -51,7 +51,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "4.9.8"
+    __version__ = "4.10.8"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
