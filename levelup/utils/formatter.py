472712ecc86d5b4a5dd713152b7be2f0b4d7438c
CptDinosaur <drdinosaur42@gmail.com>
2023-08-10 13:55:08 +0100

    Fix xp range end values, bump minor version

--- levelup/utils/formatter.py
+++ levelup/utils/formatter.py
@@ -39,7 +39,7 @@ def time_to_level(
     xp_obtained = 0
     time_to_reach_level = 0  # Seconds
     while True:
-        xp = random.choice(range(xp_range[0], xp_range[1]))
+        xp = random.choice(range(xp_range[0], xp_range[1] + 1))
         xp_obtained += xp
         # Wait up to an hour after cooldown for a little more realism
         time_to_reach_level += cooldown + random.randint(0, 3600)
