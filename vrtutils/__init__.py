cf89db155316fdfdee79d737fa55b529f71fc289
Vert <alex.c.goble@gmail.com>
2023-09-01 15:59:36 -0400

    VrtUtils - merge ping into botinfo

--- vrtutils/__init__.py
+++ vrtutils/__init__.py
@@ -1,5 +1,10 @@
+from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 
-from .vrtutils import setup  # noqa
+from .vrtutils import VrtUtils
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
+
+
+async def setup(bot: Red):
+    await bot.add_cog(VrtUtils(bot))
