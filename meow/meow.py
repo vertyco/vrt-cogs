922d9fe4472f06fb4248f4bf8ea594f7d9b83dd2
Vert <alex.c.goble@gmail.com>
2023-09-04 19:33:23 -0400

    Meow - typehinting

--- meow/meow.py
+++ meow/meow.py
@@ -28,7 +28,7 @@ class Meow(commands.Cog):
     """
 
     __author__ = "Vertyco"
-    __version__ = "0.1.1"
+    __version__ = "0.1.2"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
@@ -41,24 +41,22 @@ class Meow(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def meow(self, ctx, *, text: str = None):
+    async def meow(self, ctx: commands.Context, *, text: str = None):
         if not text:
             if hasattr(ctx.message, "reference") and ctx.message.reference:
                 try:
                     text = ctx.message.reference.resolved.content
                 except AttributeError:
                     pass
-            if not text:
-                async for msg in ctx.channel.history(limit=3):
-                    text = msg.content
-                    if text:
+            else:
+                async for msg in ctx.channel.history(limit=3, oldest_first=False):
+                    if text := msg.content:
                         break
         await self.meowstring(text, ctx)
 
-    async def meowstring(self, text, ctx):
+    async def meowstring(self, text: str, ctx: commands.Context):
         if "now" in text:
-            newstring = text.replace("now", "meow")
-            await ctx.send(newstring)
+            await ctx.send(text.replace("now", "meow"))
         else:
             return await ctx.send(self.get_cat())
 
