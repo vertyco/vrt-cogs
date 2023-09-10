ca0c117c8b77078ec16121b0df1c6daa9bc7532e
Vert <alex.c.goble@gmail.com>
2023-09-10 12:01:01 -0400

    Tickets - Fallback to channel send if interaction is unknown

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -32,7 +32,7 @@ class Tickets(TicketCommands, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "Vertyco"
-    __version__ = "2.3.0"
+    __version__ = "2.3.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
