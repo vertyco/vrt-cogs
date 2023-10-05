20b779a965ec0afd5f80b3ceec3396a54849c966
Vert <alex.c.goble@gmail.com>
2023-10-05 19:50:27 -0400

    LevelUp - handle rando bg error

--- levelup/levelup.py
+++ levelup/levelup.py
@@ -81,7 +81,7 @@ class LevelUp(UserCommands, Generator, commands.Cog, metaclass=CompositeMetaClas
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "3.8.4"
+    __version__ = "3.8.5"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
