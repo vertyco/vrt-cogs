c6333b3727d0ed8216877600c0c0c38b21bc7a84
Vert <alex.c.goble@gmail.com>
2023-10-09 12:09:55 -0400

    Assistant - only get embeddings for questions of first message

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -59,7 +59,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "4.16.1"
+    __version__ = "4.16.2"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
