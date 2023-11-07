f106efa678c35052116fde335c6dafe674cd2a59
Vert <alex.c.goble@gmail.com>
2023-11-07 14:44:43 -0500

    Assistant - openai removed embed_utils

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -60,7 +60,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "4.18.7"
+    __version__ = "4.19.0"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
