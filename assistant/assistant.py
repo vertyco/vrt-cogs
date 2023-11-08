6c86e25db488feb1c0c0a7170598e38f881ddda6
Vert <alex.c.goble@gmail.com>
2023-11-07 22:24:26 -0500

    Assisant - handle InternalServerError

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -60,7 +60,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.0.0"
+    __version__ = "5.0.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
