1dc7aa1aeb3ebb09504c06b6a408d9d3fe51661d
Vert <alex.c.goble@gmail.com>
2023-11-07 22:31:52 -0500

    Handle tool id

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -60,7 +60,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.0.1"
+    __version__ = "5.0.2"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
