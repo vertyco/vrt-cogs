a82d69b643c0c28f66f773fd3985087bc79da2b4
Vert <alex.c.goble@gmail.com>
2023-11-09 23:11:57 -0500

    Assistant - update logging handle

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -63,7 +63,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.1.7"
+    __version__ = "5.1.8"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
