f980c906e431f9fbaba0904b75f6227889261435
Vert <alex.c.goble@gmail.com>
2023-11-13 16:35:36 -0500

    Assistant - tenacity

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -64,7 +64,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.2.6"
+    __version__ = "5.2.7"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
