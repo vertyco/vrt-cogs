ce5a71561071cc14a6629ac4d8083ebcf8b01adc
Vert <alex.c.goble@gmail.com>
2023-11-14 13:16:30 -0500

    Assistant - tool call cleanup

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -64,7 +64,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.2.8"
+    __version__ = "5.2.9"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
