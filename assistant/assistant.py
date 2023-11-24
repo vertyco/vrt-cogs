8cbf1466a607305a19388575e4e524463df5c3d3
Vert <alex.c.goble@gmail.com>
2023-11-24 09:50:01 -0500

    Assistant - fix get topic function

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -64,7 +64,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.4.0"
+    __version__ = "5.4.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
