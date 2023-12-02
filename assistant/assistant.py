5487223a649dadf07b811a5d8e8b7946c804a832
Vert <alex.c.goble@gmail.com>
2023-12-02 11:55:15 -0500

    Assistant - fix convopop

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -54,7 +54,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.6.0"
+    __version__ = "5.6.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
