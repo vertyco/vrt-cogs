7d75aad3f2d84397bfdbb4e1f497e5bbb8f94aa4
Vert <alex.c.goble@gmail.com>
2023-12-18 09:06:42 -0500

    Assistant - fix max tokens for gpt vision

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -54,7 +54,7 @@ class Assistant(
     """
 
     __author__ = "Vertyco#0117"
-    __version__ = "5.7.8"
+    __version__ = "5.7.9"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
