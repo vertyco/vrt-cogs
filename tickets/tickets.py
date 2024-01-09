4c8358c6c9a7cae5c83af841f8e633521473720e
Vert <alex.c.goble@gmail.com>
2024-01-09 18:50:38 -0500

    Tickets - cleanup archival message

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -34,7 +34,7 @@ class Tickets(TicketCommands, Functions, commands.Cog, metaclass=CompositeMetaCl
     """
 
     __author__ = "Vertyco"
-    __version__ = "2.7.2"
+    __version__ = "2.7.3"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
