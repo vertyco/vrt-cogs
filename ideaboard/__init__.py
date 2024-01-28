56577c85eeec31038de4e840d47913c63b9d1f04
Vert <alex.c.goble@gmail.com>
2024-01-28 14:11:22 -0500

    IdeaBoard - initial commit

---
+++ ideaboard/__init__.py
@@ -0,0 +1,11 @@
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+
+from .main import IdeaBoard
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
+
+
+async def setup(bot: Red):
+    cog = IdeaBoard(bot)
+    await bot.add_cog(cog)
---
+++ ideaboard/commands/user.py
@@ -0,0 +1,230 @@
+import logging
+import typing as t
+from contextlib import suppress
+from datetime import datetime
+from uuid import uuid4
+
+import discord
+from discord import app_commands
+from redbot.core import commands
+from redbot.core.i18n import Translator, cog_i18n
+from redbot.core.utils.chat_formatting import humanize_timedelta
+
+from ..abc import MixinMeta
+from ..common.models import Suggestion
+from ..views.voteview import VoteView
+
+log = logging.getLogger("red.vrt.ideaboard.commands.user")
+_ = Translator("IdeaBoard", __file__)
+
+
+@cog_i18n(_)
+class User(MixinMeta):
+    @commands.hybrid_command(name="idea", aliases=["suggest"], description=_("Share an idea/make a suggestion."))
+    @app_commands.describe(content="Your idea or suggestion")
+    @commands.guild_only()
+    @commands.bot_has_permissions(manage_messages=True)
+    async def idea(self, ctx: commands.Context, *, content: str):
+        """Share an idea/make a suggestion."""
+
+        async def resp(txt: str):
+            """Return a response to the user, and delete their message if possible."""
+            if ctx.interaction:
+                try:
+                    await ctx.interaction.response.send_message(txt, ephemeral=True)
+                except discord.NotFound:
+                    await ctx.interaction.followup.send(txt, ephemeral=True)
+            else:
+                await ctx.send(txt)
+
+            with suppress(discord.NotFound):
+                await ctx.message.delete()
+
+        conf = self.db.get_conf(ctx.guild)
+        if not conf.pending:
+            txt = _("The pending suggestions channel has not been set!")
+            return await resp(txt)
+
+        channel = ctx.guild.get_channel(conf.pending)
+        if not channel:
+            txt = _("The pending suggestions channel no longer exists!")
+            return await resp(txt)
+
+        perms = [
+            channel.permissions_for(ctx.me).send_messages,
+            channel.permissions_for(ctx.me).embed_links,
+        ]
+        if not all(perms):
+            txt = _("I do not have the required permissions to send messages in the pending suggestions channel.")
+            return await resp(txt)
+
+        # Check user's permission to suggest
+        if conf.suggest_roles and not any(
+            role.id in [role.id for role in ctx.author.roles] for role in conf.suggest_roles
+        ):
+            return await resp(_("You do not have the required roles to make suggestions."))
+
+        # Check account age
+        if conf.min_account_age_to_suggest:
+            account_age = (datetime.now().astimezone() - ctx.author.created_at).total_seconds() / 3600
+            if account_age < conf.min_account_age_to_suggest:
+                return await resp(_("Your account is too young to make suggestions."))
+
+        # Check join time
+        if conf.min_join_time_to_suggest:
+            join_age = (datetime.utcnow() - ctx.author.joined_at).total_seconds() / 3600
+            if join_age < conf.min_join_time_to_suggest:
+                return await resp(_("You haven't been in the server long enough to make suggestions."))
+
+        # Check role blacklist
+        if conf.role_blacklist and any(
+            role.id in [role.id for role in ctx.author.roles] for role in conf.role_blacklist
+        ):
+            return await resp(_("You are not allowed to make suggestions due to a blacklisted role."))
+
+        # Check user blacklist
+        if conf.user_blacklist and ctx.author.id in conf.user_blacklist:
+            return await resp(_("You are blacklisted from making suggestions."))
+
+        if not conf.approvers:
+            txt = _("No approvers have been set! Admins needs to use the {} command to add one.").format(
+                f"`{ctx.clean_prefix}approverole @role`"
+            )
+            return await resp(txt)
+
+        # Check LevelUp requirement
+        try:
+            txt = _("You must be level {} or higher to make suggestions.\nUse the {} command to check your level.")
+            if conf.min_level_to_suggest and self.bot.get_cog("LevelUp"):
+                levelup = self.bot.get_cog("LevelUp")
+                if ctx.guild.id in levelup.data:
+                    levelup.init_user(ctx.guild.id, str(ctx.author.id))
+                    level = levelup.data[ctx.guild.id]["users"][str(ctx.author.id)]["level"]
+                    if level < conf.min_level_to_suggest:
+                        return await resp(txt.format(conf.min_level_to_suggest, f"`{ctx.clean_prefix}pf`"))
+        except Exception as e:
+            log.exception(f"Failed to check LevelUp requirement in {ctx.guild.name}", exc_info=e)
+
+        # Check ArkTools requirement
+        try:
+            if conf.min_playtime_to_suggest and self.bot.get_cog("ArkTools"):
+                arktools = self.bot.get_cog("ArkTools")
+                player = await arktools.db_utils.search_player_cached(ctx.guild, ctx.author)
+                if not player:
+                    txt = _("You must be registered in the ArkTools database to make suggestions.")
+                    return await resp(txt)
+                playtime_hours = player.playtime / 3600
+                if playtime_hours < conf.min_playtime_to_suggest:
+                    return await resp(
+                        _("You must have at least {} hours of playtime to make suggestions.").format(
+                            conf.min_playtime_to_suggest
+                        )
+                    )
+        except Exception as e:
+            log.exception(f"Failed to check ArkTools requirement in {ctx.guild.name}", exc_info=e)
+
+        # Check cooldowns
+        profile = conf.get_profile(ctx.author)
+        if profile.last_suggestion:
+            delta = datetime.now() - profile.last_suggestion
+            cooldown = conf.base_cooldown
+            # Check role cooldowns, use the lowest cooldown of all roles the user has
+            if conf.role_cooldowns:
+                # Fetch all cooldowns of roles the user has
+                user_roles = [r.id for r in ctx.author.roles]
+                cooldowns = {k: v for k, v in conf.role_cooldowns.items() if k in user_roles}
+                # If user has roles with cooldowns, use that instead of the base cooldown
+                if cooldowns:
+                    cooldown = min(cooldowns.values())
+
+            if delta.total_seconds() < cooldown:
+                return await resp(
+                    _("You must wait `{}` before making another suggestion.").format(
+                        humanize_timedelta(seconds=cooldown - delta.total_seconds())
+                    )
+                )
+
+        with suppress(discord.NotFound):
+            await ctx.message.delete()
+
+        suggestion_number = conf.counter + 1
+        profile.last_suggestion = datetime.now()
+
+        count = _("Suggestion #{}").format(suggestion_number)
+        # Create the suggestion embed
+        embed = discord.Embed(color=discord.Color.blurple(), description=content)
+        if conf.anonymous:
+            embed.set_footer(text=_("Posted anonymously"))
+        else:
+            text = _("Posted by {}").format(f"{ctx.author.name} ({ctx.author.id})")
+            embed.set_footer(text=text, icon_url=ctx.author.display_avatar)
+
+        suggestion_id = str(uuid4())
+        view = VoteView(self, ctx.guild, suggestion_number, suggestion_id)
+        message = await channel.send(count, embed=embed, view=view)
+
+        suggestion = Suggestion(id=suggestion_id, message_id=message.id, author_id=ctx.author.id)
+        conf.suggestions[suggestion_number] = suggestion
+        conf.counter = suggestion_number
+
+        profile.suggestions_made += 1
+
+        txt = _("Your [suggestion]({}) has been posted!").format(message.jump_url)
+
+        if ctx.interaction:
+            try:
+                await ctx.interaction.response.send_message(txt, ephemeral=True)
+            except discord.NotFound:
+                try:
+                    await ctx.interaction.followup.send(txt, ephemeral=True)
+                except discord.NotFound:
+                    try:
+                        await ctx.author.send(txt)
+                    except discord.Forbidden:
+                        await ctx.channel.send(txt)
+        else:
+            try:
+                await ctx.author.send(txt)
+            except discord.Forbidden:
+                await ctx.channel.send(txt, delete_after=10)
+
+        await self.save()
+
+    @commands.hybrid_command(
+        name="ideastats",
+        description=_("View your profile stats regarding suggestions and votes."),
+    )
+    @commands.guild_only()
+    async def view_profile(self, ctx: commands.Context, *, member: t.Optional[discord.Member] = None):
+        """Display your current profile stats for suggestions and votes."""
+        conf = self.db.get_conf(ctx.guild)
+        if not member:
+            member = ctx.author
+
+        profile = conf.get_profile(member)
+
+        embed = discord.Embed(color=discord.Color.gold(), title=_("Stats for {}").format(member.display_name))
+        embed.set_thumbnail(url=member.display_avatar)
+
+        embed.add_field(
+            name=_("Suggestion Summary"),
+            value=_("Suggestions Made: {}\nSuggestions Approved: {}\nSuggestions Denied: {}").format(
+                profile.suggestions_made,
+                profile.suggestions_approved,
+                profile.suggestions_denied,
+            ),
+            inline=False,
+        )
+
+        embed.add_field(
+            name=_("Voting Summary"),
+            value=_(
+                "Total Upvotes: {}\n"
+                "Total Downvotes: {}\n"
+                "Successful Votes (Wins): {}\n"
+                "Unsuccessful Votes (Losses): {}"
+            ).format(profile.upvotes, profile.downvotes, profile.wins, profile.losses),
+            inline=False,
+        )
+
+        await ctx.send(embed=embed)
