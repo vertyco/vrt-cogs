a57dfa0554077151d7bb2ed19f544c001f5ae935
Vert <alex.c.goble@gmail.com>
2024-01-28 16:20:03 -0500

    IdeaBoard - fix ArkTools playtime requirement

--- ideaboard/main.py
+++ ideaboard/main.py
@@ -18,7 +18,7 @@ class IdeaBoard(Commands, commands.Cog, metaclass=CompositeMetaClass):
     """Share Ideas and Suggestions"""
 
     __author__ = "Vertyco#0117"
-    __version__ = "0.0.5a"
+    __version__ = "0.0.6a"
 
     def __init__(self, bot: Red):
         super().__init__()
