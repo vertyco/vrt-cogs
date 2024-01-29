df32e243e71aedc624c37a6d25ceddf58644be66
Vert <alex.c.goble@gmail.com>
2024-01-28 21:04:25 -0500

    IdeaBoard - approvers can bypass cooldown

--- ideaboard/main.py
+++ ideaboard/main.py
@@ -18,7 +18,7 @@ class IdeaBoard(Commands, commands.Cog, metaclass=CompositeMetaClass):
     """Share Ideas and Suggestions"""
 
     __author__ = "Vertyco#0117"
-    __version__ = "0.0.8a"
+    __version__ = "0.1.0"
 
     def __init__(self, bot: Red):
         super().__init__()
