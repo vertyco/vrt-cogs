4c736d147ca0e2217bf6801f22111496e49e4c78
Vert <alex.c.goble@gmail.com>
2024-02-07 20:11:53 -0500

    NoNuke - refactor

--- nonuke/__init__.py
+++ nonuke/__init__.py
@@ -1,6 +1,3 @@
-import asyncio
-
-from redbot.core import VersionInfo, version_info
 from redbot.core.utils import get_end_user_data_statement
 
 from .nonuke import NoNuke
@@ -9,9 +6,4 @@ __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 
 async def setup(bot):
-    cog = NoNuke(bot)
-    if version_info >= VersionInfo.from_str("3.5.0"):
-        await bot.add_cog(cog)
-    else:
-        bot.add_cog(cog)
-    asyncio.create_task(cog.initialize())
+    await bot.add_cog(NoNuke(bot))
--- nonuke/nonuke.py
+++ nonuke/nonuke.py
@@ -1,19 +1,28 @@
+import asyncio
+import logging
+
 import discord
 from redbot.core import Config, commands
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import box, humanize_list
 
-from .listen import Listen
+from .abc import CompositeMetaClass
+from .common.listen import Listen
+from .common.models import DB
+
+log = logging.getLogger("red.vrt.nonuke")
 
 
-class NoNuke(Listen, commands.Cog):
+class NoNuke(Listen, commands.Cog, metaclass=CompositeMetaClass):
     """
     Anti-Nuke System for lazy guild owners!
 
     Monitors the following events:
     Kicks & Bans
-    Channel Creation/Edit/Deletion
+    Channel/Thread Creation/Edit/Deletion
     Role Creation/Edit/Deletion
+    Emoji/Webhook Deletion
+    Member role updates
 
     Set a cooldown(in seconds)
     Set an overload count(X events in X seconds)
@@ -23,9 +32,9 @@ class NoNuke(Listen, commands.Cog):
     """
 
     __author__ = "Vertyco"
-    __version__ = "0.3.0"
+    __version__ = "1.0.0"
 
-    def format_help_for_context(self, ctx):
+    def format_help_for_context(self, ctx: commands.Context):
         helpcmd = super().format_help_for_context(ctx)
         return f"{helpcmd}\nCog Version: {self.__version__}\nAuthor: {self.__author__}"
 
@@ -36,37 +45,55 @@ class NoNuke(Listen, commands.Cog):
         super().__init__()
         self.bot = bot
         self.config = Config.get_conf(self, 117, force_registration=True)
+        self.config.register_global(db={})
+        self.db: DB = DB()
+        self.saving = False
 
-        default_guild = {
-            "enabled": False,
-            "log": 0,  # Log channel
-            "cooldown": 10,  # Seconds in between actions
-            "overload": 3,  # Actions within cooldown time
-            "dm": False,  # Whether to DM the user the bot kicks
-            "action": "notify",  # Valid types are 'kick', 'ban', 'strip', and 'notify'
-            "ignore_bots": False,  # Whether to ignore other bots
-            "whitelist": [],  # Whitelist of trusted users(or bots)
-        }
-        self.config.register_guild(**default_guild)
+    async def cog_load(self) -> None:
+        asyncio.create_task(self.initialize())
 
-        self.settings = {}
-        self.cache = {}
-        self.first_run = True
+    async def save(self) -> None:
+        if self.saving:
+            return
+        try:
+            self.saving = True
+            dump = await asyncio.to_thread(self.db.model_dump, mode="json")
+            await self.config.db.set(dump)
+        except Exception as e:
+            log.exception("Failed to save config", exc_info=e)
+        finally:
+            self.saving = False
 
-    async def initialize(self, guild_id: int = 0):
+    async def initialize(self):
         await self.bot.wait_until_red_ready()
-        conf = await self.config.all_guilds()
-        for gid, settings in conf.items():
-            if not settings:
-                continue
-            if guild_id and gid != guild_id:
-                continue
-            self.settings[gid] = settings
+        data = await self.config.db()
+
+        # Pre v1.0.0 migration
+        save = False
+        if not data:
+            log.warning("PERFORMING MIGRATION")
+            conf = await self.config.all_guilds()
+
+            configs = {}
+            for gid, settings in conf.items():
+                if not settings:
+                    continue
+                configs[gid] = settings
+
+            await self.config.clear_all_guilds()
+            data["configs"] = configs
+            save = True
+
+        # Validate model
+        self.db = await asyncio.to_thread(DB.model_validate, data)
+        log.info("Config loaded")
+        if save:
+            await self.save()
 
     @commands.group()
     @commands.guildowner()
     @commands.guild_only()
-    async def nonuke(self, ctx):
+    async def nonuke(self, ctx: commands.Context):
         """
         Anti-Nuke System for lazy guild owners!
 
@@ -83,19 +110,19 @@ class NoNuke(Listen, commands.Cog):
         """
 
     @nonuke.command()
-    async def enable(self, ctx):
+    async def enable(self, ctx: commands.Context):
         """Enable/Disable the NoNuke system"""
-        toggle = await self.config.guild(ctx.guild).enabled()
-        if toggle:
-            await self.config.guild(ctx.guild).enabled.set(False)
+        conf = self.db.get_conf(ctx.guild)
+        if conf.enabled:
+            conf.enabled = False
             await ctx.send("NoNuke system **Disabled**")
         else:
-            await self.config.guild(ctx.guild).enabled.set(True)
+            conf.enabled = True
             await ctx.send("NoNuke system **Enabled**")
-        await self.initialize(ctx.guild)
+        await self.save()
 
     @nonuke.command()
-    async def ignorebots(self, ctx):
+    async def ignorebots(self, ctx: commands.Context):
         """
         Toggle whether other bots are ignored
 
@@ -103,42 +130,42 @@ class NoNuke(Listen, commands.Cog):
         If NoNuke is set to strip roles, and a bot triggers it while having an integrated role, NoNuke will fail
         to remove the role from it.
         """
-        toggle = await self.config.guild(ctx.guild).ignore_bots()
-        if toggle:
-            await self.config.guild(ctx.guild).ignore_bots.set(False)
+        conf = self.db.get_conf(ctx.guild)
+        if conf.ignore_bots:
+            conf.ignore_bots = False
             await ctx.send("Other bots will **not** be ignored")
         else:
-            await self.config.guild(ctx.guild).ignore_bots.set(True)
+            conf.ignore_bots = True
             await ctx.send("Other bots will be ignored")
-        await self.initialize(ctx.guild)
+        await self.save()
 
     @nonuke.command()
-    async def dm(self, ctx):
+    async def dm(self, ctx: commands.Context):
         """Toggle whether the bot sends the user a DM when a kick or ban action is performed"""
-        toggle = await self.config.guild(ctx.guild).dm()
-        if toggle:
-            await self.config.guild(ctx.guild).dm.set(False)
+        conf = self.db.get_conf(ctx.guild)
+        if conf.dm:
+            conf.dm = False
             await ctx.send("NoNuke trigger DM **Disabled**")
         else:
-            await self.config.guild(ctx.guild).dm.set(True)
+            conf.dm = True
             await ctx.send("NoNuke trigger DM **Enabled**")
-        await self.initialize(ctx.guild)
+        await self.save()
 
     @nonuke.command()
     async def logchannel(self, ctx: commands.Context, channel: discord.TextChannel):
         """Set the log channel for Anti-Nuke kicks"""
         if not channel.permissions_for(ctx.me).embed_links:
             return await ctx.send("I dont have permission to send embeds in that channel!")
-        await self.config.guild(ctx.guild).log.set(channel.id)
+        self.db.get_conf(ctx.guild).log = channel.id
         await ctx.tick()
-        await self.initialize(ctx.guild)
+        await self.save()
 
     @nonuke.command()
     async def cooldown(self, ctx: commands.Context, cooldown: int):
         """Cooldown (in seconds) for NoNuke to trigger"""
-        await self.config.guild(ctx.guild).cooldown.set(cooldown)
+        self.db.get_conf(ctx.guild).cooldown = cooldown
         await ctx.tick()
-        await self.initialize(ctx.guild)
+        await self.save()
 
     @nonuke.command()
     async def overload(self, ctx: commands.Context, overload: int):
@@ -151,7 +178,8 @@ class NoNuke(Listen, commands.Cog):
         """
         if not ctx.guild.me.guild_permissions.view_audit_log:
             return await ctx.send("I do not have permission to view the audit log for this server!")
-        action = await self.config.guild(ctx.guild).action()
+        conf = self.db.get_conf(ctx.guild)
+        action = conf.action
         if action == "kick":
             if not ctx.guild.me.guild_permissions.kick_members:
                 return await ctx.send("I do not have permission to kick members!")
@@ -161,9 +189,9 @@ class NoNuke(Listen, commands.Cog):
         elif action == "strip":
             if not ctx.guild.me.guild_permissions.manage_roles:
                 return await ctx.send("I do not have permission to manage roles!")
-        await self.config.guild(ctx.guild).overload.set(overload)
+        conf.overload = overload
         await ctx.tick()
-        await self.initialize(ctx.guild)
+        await self.save()
 
     @nonuke.command()
     async def action(self, ctx: commands.Context, action: str):
@@ -190,36 +218,37 @@ class NoNuke(Listen, commands.Cog):
         elif action == "strip":
             if not ctx.guild.me.guild_permissions.manage_roles:
                 return await ctx.send("I do not have permission to manage roles!")
-        await self.config.guild(ctx.guild).action.set(action)
+        self.db.get_conf(ctx.guild).action = action
         await ctx.tick()
-        await ctx.send(f"Action has been set to `{action}`")
-        await self.initialize(ctx.guild)
+        await self.save()
 
     @nonuke.command()
     async def whitelist(self, ctx: commands.Context, user: discord.Member):
         """Add/Remove users from the whitelist"""
-        async with self.config.guild(ctx.guild).whitelist() as whitelist:
-            if user.id in whitelist:
-                whitelist.remove(user.id)
-                await ctx.send(f"{user} has been removed from the whitelist!")
-            else:
-                whitelist.append(user.id)
-                await ctx.send(f"{user} has been added to the whitelist!")
+        conf = self.db.get_conf(ctx.guild)
+        if user.id in conf.whitelist:
+            conf.whitelist.remove(user.id)
+            await ctx.send(f"{user} has been removed from the whitelist!")
+        else:
+            conf.whitelist.append(user.id)
+            await ctx.send(f"{user} has been added to the whitelist!")
+        await self.save()
 
     @nonuke.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def view(self, ctx):
+    async def view(self, ctx: commands.Context):
         """View the NoNuke settings"""
-        conf = await self.config.guild(ctx.guild).all()
-        lchan = self.bot.get_channel(conf["log"]) if conf["log"] else "Not Set"
+        conf = self.db.get_conf(ctx.guild)
+
+        lchan = self.bot.get_channel(conf.log) if conf.log else "Not Set"
         em = discord.Embed(
             title="NoNuke Settings",
-            description=f"`Enabled:    `{conf['enabled']}\n"
-            f"`Cooldown:   `{conf['cooldown']}\n"
-            f"`Overload:   `{conf['overload']}\n"
-            f"`DM:         `{conf['dm']}\n"
-            f"`Action:     `{conf['action']}\n"
-            f"`IgnoreBots: `{conf['ignore_bots']}\n"
+            description=f"`Enabled:    `{conf.enabled}\n"
+            f"`Cooldown:   `{conf.cooldown}\n"
+            f"`Overload:   `{conf.overload}\n"
+            f"`DM:         `{conf.dm}\n"
+            f"`Action:     `{conf.action}\n"
+            f"`IgnoreBots: `{conf.ignore_bots}\n"
             f"`LogChannel: `{lchan}",
         )
         await ctx.send(embed=em)
