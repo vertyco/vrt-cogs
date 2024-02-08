58709ae03788acd35f845a1c6b3c0827f678a179
Vert <alex.c.goble@gmail.com>
2024-02-07 22:18:45 -0500

    NoNuke - dont include role updates

--- nonuke/nonuke.py
+++ nonuke/nonuke.py
@@ -34,7 +34,7 @@ class NoNuke(Listen, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "Vertyco"
-    __version__ = "1.1.0"
+    __version__ = "1.1.1"
 
     def format_help_for_context(self, ctx: commands.Context):
         helpcmd = super().format_help_for_context(ctx)
