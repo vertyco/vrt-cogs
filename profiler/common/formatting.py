b50f761e8284c4abf28ef0bfff0229631241d306
Vert <alex.c.goble@gmail.com>
2024-02-17 14:16:56 -0500

    Profiler - cleanup, adjust delta

--- profiler/common/formatting.py
+++ profiler/common/formatting.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from redbot.core.utils.chat_formatting import box
 from tabulate import tabulate
 
-from .models import StatsProfile
+from .models import DB, StatsProfile
 
 
 def format_method_pages(method_key: str, data: t.List[StatsProfile]) -> t.List[str]:
@@ -40,12 +40,13 @@ def format_method_tables(data: t.List[StatsProfile]) -> t.List[str]:
 
 
 def format_runtime_pages(
-    data: t.Dict[str, t.Dict[str, t.List[StatsProfile]]],
+    db: DB,
     sort_by: str,
     query: str = None,
 ) -> t.List[str]:
+    now = datetime.now()
     stats: t.Dict[str, list] = {}
-    for methodlist in data.values():
+    for methodlist in db.stats.values():
         for method_key, profiles in methodlist.items():
             if query and query not in method_key:
                 continue
@@ -56,35 +57,44 @@ def format_runtime_pages(
             max_runtime = max(profile.total_tt for profile in profiles)
             min_runtime = min(profile.total_tt for profile in profiles)
             avg_runtime = sum(profile.total_tt for profile in profiles) / len(profiles)
-            # Calculate calls to this function per minute for the last hour
-            calls_last_hour = [prof for prof in profiles if prof.timestamp > (datetime.now() - timedelta(hours=1))]
-            calls_per_minute = len(calls_last_hour) / 60
-            total_calls = len(calls_last_hour)
+
+            # Calculate the calls per minute and total calls in the last specified delta
+            calls_over_delta = [i for i in profiles if i.timestamp > (now - timedelta(hours=db.delta))]
+            # Caclulate the min and max timestamp
+            newest, oldest = (
+                max(calls_over_delta, key=lambda i: i.timestamp),
+                min(calls_over_delta, key=lambda i: i.timestamp),
+            )
+            timeframe_minutes = (newest.timestamp - oldest.timestamp).total_seconds() / 60
+            # Now calculate the calls per minute
+            calls_per_minute = len(calls_over_delta) / timeframe_minutes if timeframe_minutes else 0
+            total_calls = len(calls_over_delta)
             stats[method_key] = [max_runtime, min_runtime, avg_runtime, calls_per_minute, total_calls]
 
     per_page = 10
     start = 0
     end = per_page
     page_count = math.ceil(len(stats) / per_page)
+    delta_text = f"Last {'Hour' if db.delta == 1 else f'{db.delta}hrs'}"
 
-    cols = ["Method", "Max", "Min", "Avg", "Calls/Min", "Last Hour Calls"]
+    cols = ["Method", "Max", "Min", "Avg", "Calls/Min", delta_text]
     if sort_by == "Name":
-        cols = ["[Method]", "Max", "Min", "Avg", "Calls/Min", "Last Hour Calls"]
+        cols = ["[Method]", "Max", "Min", "Avg", "Calls/Min", delta_text]
         stats = dict(sorted(stats.items()))
     elif sort_by == "Max":
-        cols = ["Method", "[Max]", "Min", "Avg", "Calls/Min", "Last Hour Calls"]
+        cols = ["Method", "[Max]", "Min", "Avg", "Calls/Min", delta_text]
         stats = dict(sorted(stats.items(), key=lambda item: item[1][0], reverse=True))
     elif sort_by == "Min":
-        cols = ["Method", "Max", "[Min]", "Avg", "Calls/Min", "Last Hour Calls"]
+        cols = ["Method", "Max", "[Min]", "Avg", "Calls/Min", delta_text]
         stats = dict(sorted(stats.items(), key=lambda item: item[1][1], reverse=True))
     elif sort_by == "Avg":
-        cols = ["Method", "Max", "Min", "[Avg]", "Calls/Min", "Last Hour Calls"]
+        cols = ["Method", "Max", "Min", "[Avg]", "Calls/Min", delta_text]
         stats = dict(sorted(stats.items(), key=lambda item: item[1][2], reverse=True))
     elif sort_by == "CPM":
-        cols = ["Method", "Max", "Min", "Avg", "[Calls/Min]", "Last Hour Calls"]
+        cols = ["Method", "Max", "Min", "Avg", "[Calls/Min]", delta_text]
         stats = dict(sorted(stats.items(), key=lambda item: item[1][3], reverse=True))
     elif sort_by == "LHC":
-        cols = ["Method", "Max", "Min", "Avg", "Calls/Min", "[Last Hour Calls]"]
+        cols = ["Method", "Max", "Min", "Avg", "Calls/Min", f"[{delta_text}]"]
         stats = dict(sorted(stats.items(), key=lambda item: item[1][4], reverse=True))
 
     def _format(value: float):
@@ -164,3 +174,30 @@ def format_runtimes(stats: t.Dict[str, t.List[StatsProfile]]):
             rows.append([method_key, exe_time, profile.is_command, profile.is_coro])
     cols = ["Method", "Time", "Command", "Coro"]
     return tabulate(rows, headers=cols)
+
+
+def timedelta_format(delta: t.Optional[timedelta] = None, seconds: t.Optional[int] = None) -> str:
+    try:
+        obj = seconds if seconds is not None else delta.total_seconds()
+    except AttributeError:
+        raise ValueError("You must provide either a timedelta or a number of seconds")
+
+    seconds = int(obj)
+    periods = [
+        ("Yr", 60 * 60 * 24 * 365),
+        ("M", 60 * 60 * 24 * 30),
+        ("d", 60 * 60 * 24),
+        ("h", 60 * 60),
+        ("m", 60),
+        ("s", 1),
+    ]
+
+    strings = []
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            if period_value == 0:
+                continue
+            strings.append(f"{period_value}{period_name}")
+
+    return ", ".join(strings)
