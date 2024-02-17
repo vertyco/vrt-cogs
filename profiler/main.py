82d02baa5d737aeb822bf44c81cb5c0716a27d65
Vert <alex.c.goble@gmail.com>
2024-02-17 00:59:10 -0500

    Profiler - menu formatting

--- profiler/main.py
+++ profiler/main.py
@@ -23,7 +23,7 @@ class Profiler(Owner, commands.Cog, metaclass=CompositeMetaClass):
     """Cog profiling tools for bot owners and developers"""
 
     __author__ = "vertyco"
-    __version__ = "0.0.8a"
+    __version__ = "0.0.9a"
 
     def __init__(self, bot: Red):
         super().__init__()
