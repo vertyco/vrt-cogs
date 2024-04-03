31d7c21d367e7770b0dc687f4f4184768bfd5a7d
Vert <alex.c.goble@gmail.com>
2024-04-03 19:14:16 -0400

    BankEvents - remove redundant deposit/withdraw dispatches

--- bankevents/overrides/bank.py
+++ bankevents/overrides/bank.py
@@ -149,42 +149,11 @@ async def transfer_credits(
         currency = await bank.get_currency_name(guild)
         raise bank.errors.BalanceTooHigh(user=to.display_name, max_balance=max_bal, currency_name=currency)
 
-    sender_new = await withdraw_credits(from_, amount)
-    recipient_new = await deposit_credits(to, amount)
+    sender_new = await bank.withdraw_credits(from_, amount)
+    recipient_new = await bank.deposit_credits(to, amount)
     payload = BankTransferInformation(from_, to, guild, amount, sender_new, recipient_new)
     _bot_ref.dispatch("red_bank_transfer_credits", payload)
     return recipient_new
-
-
-async def withdraw_credits(member: discord.Member, amount: int) -> int:
-    if not isinstance(amount, int):
-        raise TypeError("Withdrawal amount must be of type int, not {}.".format(type(amount)))
-    if bank._invalid_amount(amount):
-        raise ValueError("Invalid withdrawal amount {} < 0".format(humanize_number(amount, override_locale="en_US")))
-
-    bal = await bank.get_balance(member)
-    if amount > bal:
-        raise ValueError(
-            "Insufficient funds {} > {}".format(
-                humanize_number(amount, override_locale="en_US"),
-                humanize_number(bal, override_locale="en_US"),
-            )
-        )
-    payload = BankWithdrawDepositInformation(member, member.guild, amount, bal, bal - amount)
-    _bot_ref.dispatch("red_bank_withdraw_credits", payload)
-    return await set_balance(member, bal - amount)
-
-
-async def deposit_credits(member: discord.Member, amount: int) -> int:
-    if not isinstance(amount, int):
-        raise TypeError("Deposit amount must be of type int, not {}.".format(type(amount)))
-    if bank._invalid_amount(amount):
-        raise ValueError("Invalid deposit amount {} <= 0".format(humanize_number(amount, override_locale="en_US")))
-
-    bal = await bank.get_balance(member)
-    payload = BankWithdrawDepositInformation(member, member.guild, amount, bal, bal + amount)
-    _bot_ref.dispatch("red_bank_deposit_credits", payload)
-    return await set_balance(member, amount + bal)
 
 
 async def wipe_bank(guild: Optional[discord.Guild] = None) -> None:
