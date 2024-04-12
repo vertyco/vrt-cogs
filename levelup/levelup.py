92d187dbacf03b08c1ddfa629ff5b63314b3a8b6
Vert <alex.c.goble@gmail.com>
2024-04-12 12:32:30 -0400

    LevelUp - image validation

--- levelup/levelup.py
+++ levelup/levelup.py
@@ -81,7 +81,7 @@ class LevelUp(UserCommands, Generator, commands.Cog, metaclass=CompositeMetaClas
     """
 
     __author__ = "vertyco"
-    __version__ = "3.13.6"
+    __version__ = "3.13.7"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
