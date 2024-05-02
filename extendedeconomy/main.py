98aa92c434bae968dabb23d9f5becf8d4953d7ed
Vert <alex.c.goble@gmail.com>
2024-05-01 20:41:06 -0400

    ExtendedEconomy - fix cost check

--- extendedeconomy/main.py
+++ extendedeconomy/main.py
@@ -23,7 +23,7 @@ class ExtendedEconomy(Commands, Checks, Listeners, commands.Cog, metaclass=Compo
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "0.1.3b"
+    __version__ = "0.1.4b"
 
     def __init__(self, bot: Red):
         super().__init__()
