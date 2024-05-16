984edfa9c0193179a8096c747abff17e72538e6f
Vert <alex.c.goble@gmail.com>
2024-05-15 20:48:17 -0400

    VrtUtils - use thread

--- vrtutils/commands/logs.py
+++ vrtutils/commands/logs.py
@@ -1,3 +1,4 @@
+import asyncio
 import re
 import typing as t
 from pathlib import Path
@@ -15,21 +16,26 @@ class Logs(MixinMeta):
     @commands.is_owner()
     async def scroll_logs(self, ctx: commands.Context, max_pages: int = 20):
         """View the bot's logs."""
-        core = core_data_path()
-        log_dir = core / "logs"
-        logs: t.List[Path] = []
-        for file in log_dir.iterdir():
-            if re.match(r"latest(?:-part(?P<part>\d+))?\.log", file.name):
-                logs.append(file)
-        logs.sort(reverse=True)
-        # Combine contents of all log files
-        combined = "\n".join(log.read_text(encoding="utf-8", errors="ignore") for log in logs)
-        split = [i for i in pagify(combined, page_length=1800)]
-        split.reverse()
-        split = split[:max_pages]
-        pages = []
-        for idx, chunk in enumerate(split):
-            foot = f"Page {idx + 1}/{len(split)}"
-            txt = f"{box(chunk, lang='python')}\n{foot}"
-            pages.append(txt)
+
+        def _get_logs() -> t.List[str]:
+            core = core_data_path()
+            log_dir = core / "logs"
+            logs: t.List[Path] = []
+            for file in log_dir.iterdir():
+                if re.match(r"latest(?:-part(?P<part>\d+))?\.log", file.name):
+                    logs.append(file)
+            logs.sort(reverse=True)
+            # Combine contents of all log files
+            combined = "\n".join(log.read_text(encoding="utf-8", errors="ignore") for log in logs)
+            split = [i for i in pagify(combined, page_length=1800)]
+            split.reverse()
+            split = split[:max_pages]
+            pages = []
+            for idx, chunk in enumerate(split):
+                foot = f"Page {idx + 1}/{len(split)}"
+                txt = f"{box(chunk, lang='python')}\n{foot}"
+                pages.append(txt)
+            return pages
+
+        pages = await asyncio.to_thread(_get_logs)
         await DynamicMenu(ctx.author, pages, ctx.channel, timeout=7200).refresh()
