1db02ade9a212da9df714dd8164737a38257d7bd
Vert <alex.c.goble@gmail.com>
2024-05-31 21:47:05 -0400

    Profiler - easy way to clear all metrics

--- profiler/main.py
+++ profiler/main.py
@@ -25,7 +25,7 @@ class Profiler(Owner, Profiling, Wrapper, commands.Cog, metaclass=CompositeMetaC
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "1.4.4"
+    __version__ = "1.4.5"
 
     def __init__(self, bot: Red):
         super().__init__()
