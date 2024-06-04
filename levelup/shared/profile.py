bdc2a1b12ce4e875323bcbcb9ff2f9ceb1b455a0
Vert <alex.c.goble@gmail.com>
2024-06-04 09:20:16 -0400

    fix font path

--- levelup/shared/profile.py
+++ levelup/shared/profile.py
@@ -183,9 +183,9 @@ class ProfileFormatting(MixinMeta):
             kwargs["role_icon"] = await role_icon.read()
         if profile.font:
             if (self.fonts / profile.font).exists():
-                kwargs["font_bytes"] = str(self.fonts / profile.font)
+                kwargs["font_path"] = str(self.fonts / profile.font)
             elif (self.custom_fonts / profile.font).exists():
-                kwargs["font_bytes"] = str(self.custom_fonts / profile.font)
+                kwargs["font_path"] = str(self.custom_fonts / profile.font)
 
         endpoints = {
             "default": "fullprofile",
