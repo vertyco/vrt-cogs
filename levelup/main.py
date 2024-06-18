06bdbe9a7c5d8bcf6c135ba34d5c9bde1d48a068
Vert <alex.c.goble@gmail.com>
2024-06-18 16:28:59 -0400

    cleanup

--- levelup/main.py
+++ levelup/main.py
@@ -1,3 +1,25 @@
+"""MIT License
+
+Copyright (c) 2021-present vertyco
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 import asyncio
 import logging
 import multiprocessing as mp
@@ -89,7 +111,7 @@ class LevelUp(
         # Tenor API
         self.tenor: TenorAPI = None
 
-        # Imgen API
+        # Internal Profile Generator API
         self.api_proc: t.Union[asyncio.subprocess.Process, mp.Process] = None
 
     async def cog_load(self) -> None:
@@ -173,7 +195,8 @@ class LevelUp(
                     await self.bot.send_to_owners(
                         _(
                             "LevelUp has successfully migrated to v4!\n"
-                            "Leveling is now disabled by default and must be toggled on in each server via `[p]lset toggle`."
+                            "Leveling is now disabled by default and must be toggled on in each server via `[p]lset toggle`.\n"
+                            "[View the changelog](https://github.com/vertyco/vrt-cogs/blob/main/levelup/CHANGELOG.md) for more information."
                         )
                     )
                 except Exception as e:
