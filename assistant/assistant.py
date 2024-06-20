e48c15ba848a734fe5138e95c21b65abc8699f85
Vert <alex.c.goble@gmail.com>
2024-06-20 15:32:58 -0400

    Assistant - include member object for function prep

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -47,7 +47,7 @@ class Assistant(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "6.3.0"
+    __version__ = "6.3.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
