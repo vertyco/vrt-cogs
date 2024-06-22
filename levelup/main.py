f18887bb4eeabd89ce36dbed9b9036e8e6e8f049
Vert <alex.c.goble@gmail.com>
2024-06-22 14:21:18 -0400

    LevelUp - handle checkpoints better, preemptive naming of tasks for dpy2.4.0

--- levelup/main.py
+++ levelup/main.py
@@ -75,7 +75,7 @@ class LevelUp(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "4.0.11"
+    __version__ = "4.0.13"
     __contributors__ = [
         "[aikaterna](https://github.com/aikaterna/aikaterna-cogs)",
         "[AAA3A](https://github.com/AAA3A-AAA3A/AAA3A-cogs)",
@@ -106,7 +106,7 @@ class LevelUp(
         self.backgrounds = self.bundled_path / "backgrounds"
 
         # Save State
-        self.saving = False
+        self.io_lock = asyncio.Lock()
         self.last_save: float = perf_counter()
         self.initialized: bool = False
         self.save_tasks: t.List[asyncio.Task] = []
@@ -139,29 +139,27 @@ class LevelUp(
             self.api_proc.terminate()
 
     def save(self) -> None:
-        if self.saving:
-            return
-        if not self.initialized:
-            # Do not save if not initialized, we don't want to overwrite the config with default data
-            log.error("Config not initialized, not saving!")
-            return
-
         async def _save():
+            if self.io_lock.locked():
+                # Already saving, skip this
+                return
+            if not self.initialized:
+                # Do not save if not initialized, we don't want to overwrite the config with default data
+                log.error("Config not initialized, not saving!")
+                return
             try:
-                self.saving = True
                 log.debug("Saving config")
-                await asyncio.to_thread(
-                    self.db.to_file,
-                    path=self.settings_file,
-                    max_backups=self.db.max_backups,
-                    interval=self.db.backup_interval,
-                )
+                async with self.io_lock:
+                    await asyncio.to_thread(
+                        self.db.to_file,
+                        path=self.settings_file,
+                        max_backups=self.db.max_backups,
+                        interval=self.db.backup_interval,
+                    )
                 self.last_save = perf_counter()
                 log.debug("Config saved")
             except Exception as e:
                 log.error("Failed to save config", exc_info=e)
-            finally:
-                self.saving = False
 
         self.save_tasks.append(asyncio.create_task(_save()))
 
