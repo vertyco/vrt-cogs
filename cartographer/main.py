70c19a4d463fcfd7adba536ba421c55b343429d0
Vert <alex.c.goble@gmail.com>
2024-07-29 15:22:16 -0400

    Cartographer - keep as path object

--- cartographer/main.py
+++ cartographer/main.py
@@ -42,7 +42,7 @@ class Cartographer(commands.Cog):
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "1.0.0"
+    __version__ = "1.0.1"
 
     def __init__(self, bot: Red):
         super().__init__()
