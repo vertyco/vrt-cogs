0890073d6febbd1d7034ff9142deeb336a0c26ac
Vert <alex.c.goble@gmail.com>
2024-08-09 00:08:30 -0400

    VrtUtils - edit message bugfixes for embeds and webhooks

--- vrtutils/commands/todo.py
+++ vrtutils/commands/todo.py
@@ -17,7 +17,7 @@ class EditModal(discord.ui.Modal):
     def __init__(self, message: discord.Message):
         super().__init__(title="Edit Message", timeout=240)
         self.message = message
-        self.content = message.content
+        self.content: str | None = message.content
         self.field = discord.ui.TextInput(
             label="Message Content",
             style=discord.TextStyle.paragraph,
@@ -25,12 +25,15 @@ class EditModal(discord.ui.Modal):
             default=message.content,
             required=False,
         )
-        self.add_item(self.field)
+        if self.content:
+            self.add_item(self.field)
 
         # For other parts of the message
         self.inputs: dict[str, str] | None = None
         self.fields: dict[str, discord.ui.TextInput] = {}
         self.extras = 0
+        if self.content:
+            self.extras += 1
         if len(message.embeds) == 1:
             embed = message.embeds[0]
             for val in self.included_embed_fields:
@@ -53,7 +56,7 @@ class EditModal(discord.ui.Modal):
                     default=current,
                     required=False,
                 )
-                if self.extras < 4:
+                if self.extras < 5:
                     self.add_item(field)
                     self.fields[val] = field
                     self.extras += 1
@@ -79,8 +82,9 @@ class EditModal(discord.ui.Modal):
     async def on_submit(self, interaction: discord.Interaction):
         self.content = self.field.value
         if self.content == self.message.content:
-            return await interaction.response.send_message("No changes were made.", ephemeral=True)
-        await interaction.response.defer(ephemeral=True)
+            await interaction.response.send_message("No changes were made.", ephemeral=True)
+        else:
+            await interaction.response.defer(ephemeral=True)
         self.stop()
 
 
@@ -90,8 +94,8 @@ async def mock_edit_message(interaction: discord.Interaction, message: discord.M
     if not member:
         return await interaction.response.send_message("You must be in the server to use this command.", ephemeral=True)
     bot: Red = interaction.client
-    bots_message = message.author.id == bot.user.id
-    if not bots_message and not message.webhook_id:
+    bots_own_message = message.author.id == bot.user.id
+    if not bots_own_message and not message.webhook_id:
         # User is human, we'll need to check permissions
         if not message.channel.permissions_for(message.guild.me).manage_messages:
             return await interaction.response.send_message(
@@ -106,12 +110,15 @@ async def mock_edit_message(interaction: discord.Interaction, message: discord.M
                 "I require manage webhooks permission to edit messages.", ephemeral=True
             )
         # If user is trying to edit the message of a user who outranks them, deny
-        if member.top_role <= message.author.top_role:
+        if (
+            member.top_role <= message.author.top_role
+            and not message.channel.permissions_for(interaction.guild.me).administrator
+        ):
             return await interaction.response.send_message(
                 "You cannot edit the messages of users with equal or higher roles than you.", ephemeral=True
             )
     else:
-        if not await bot.is_admin(member):
+        if not await bot.is_admin(member) and interaction.user.id not in bot.owner_ids:
             return await interaction.response.send_message("You must be an admin to edit my messages!", ephemeral=True)
 
     # Throw a modal to edit the message
@@ -119,14 +126,14 @@ async def mock_edit_message(interaction: discord.Interaction, message: discord.M
     await interaction.response.send_modal(modal)
     await modal.wait()
 
-    if bots_message:
+    if bots_own_message:
         await message.edit(content=modal.content, embeds=modal.embeds())
     elif message.webhook_id:
         webhook = await bot.fetch_webhook(message.webhook_id)
         await webhook.edit_message(message.id, content=modal.content, embeds=modal.embeds())
     else:
-        # User is human, we'll need to mock with a webhook
-        if not modal.content:
+        # User is human or another bot, we'll need to mock with a webhook
+        if not modal.content and not message.author.bot:
             return await interaction.followup.send("User messages must have content.", ephemeral=True)
         webhooks = await message.channel.webhooks()
         if not webhooks:
@@ -134,10 +141,9 @@ async def mock_edit_message(interaction: discord.Interaction, message: discord.M
         else:
             webhook = webhooks[0]
 
-        await webhook.edit_message()
-
         await webhook.send(
             content=modal.content,
+            embeds=modal.embeds(),
             username=message.author.display_name,
             avatar_url=message.author.display_avatar.url,
             files=[await a.to_file() for a in message.attachments],
