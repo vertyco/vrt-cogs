ca5de2fb7be0fda2be64eced3784b1944567fdb9
Vert <alex.c.goble@gmail.com>
2024-10-01 19:06:22 -0400

    Assistant - update default models. remove depricated models, fix param name bug

--- assistant/common/constants.py
+++ assistant/common/constants.py
@@ -7,7 +7,7 @@ MODELS = {
     # "gpt-3.5-turbo-16k-0613": 16384 - Depricated
     # "gpt-3.5-turbo-instruct": 8192, - Depricated
     "gpt-4": 8192,
-    # "gpt-4-32k": 32768,
+    # "gpt-4-32k": 32768, - Depricated
     "gpt-4-turbo": 128000,
     "gpt-4-turbo-preview": 128000,
     "gpt-4-1106-preview": 128000,
