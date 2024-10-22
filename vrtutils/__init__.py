dcb387f185e77f6383c39748cc0395320a58b877
Vert <alex.c.goble@gmail.com>
2024-10-22 14:40:48 -0400

    VrtUtils - use teardown method instead of cog_unload

--- vrtutils/__init__.py
+++ vrtutils/__init__.py
@@ -1,6 +1,7 @@
 from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 
+from .commands.todo import mock_edit_message
 from .vrtutils import VrtUtils
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
@@ -8,3 +9,8 @@ __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot: Red):
     await bot.add_cog(VrtUtils(bot))
+    bot.tree.add_command(mock_edit_message)
+
+
+async def teardown(bot: Red):
+    bot.tree.remove_command(mock_edit_message)
