a36f5ac7f9ed6a2ba5904ebfe3db92b80c970126
Vert <alex.c.goble@gmail.com>
2024-10-30 18:17:08 -0400

    Assistant - tldr improvements

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -54,7 +54,7 @@ class Assistant(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "6.8.38"
+    __version__ = "6.8.39"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
