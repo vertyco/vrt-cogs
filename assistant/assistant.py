abd18ed45af07e6bad4c659ee7518b790f53048c
Vert <alex.c.goble@gmail.com>
2024-10-29 22:22:03 -0400

    Assistant - fix private toggle

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -54,7 +54,7 @@ class Assistant(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "6.8.36"
+    __version__ = "6.8.37"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
