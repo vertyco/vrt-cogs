ae8d141d288549234a3a1b252af8e1458dd06cb1
Vert <alex.c.goble@gmail.com>
2024-11-05 18:39:22 -0500

    CowClicker - moo no longer ephemeral

--- cowclicker/main.py
+++ cowclicker/main.py
@@ -25,7 +25,7 @@ class CowClicker(Commands, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "0.0.14b"
+    __version__ = "0.0.15b"
 
     def __init__(self, bot: Red):
         super().__init__()
