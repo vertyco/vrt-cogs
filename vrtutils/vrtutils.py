2c58397f8583bf91ce9f39091fa4a3750ea6f746
Vert <alex.c.goble@gmail.com>
2024-12-09 21:55:45 -0500

    VrtUtils - security patch for edit cooldown command, check if user has manage_channel permissions before executing

--- vrtutils/vrtutils.py
+++ vrtutils/vrtutils.py
@@ -16,7 +16,7 @@ class VrtUtils(Utils, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "2.11.6"
+    __version__ = "2.11.7"
 
     def format_help_for_context(self, ctx: commands.Context):
         helpcmd = super().format_help_for_context(ctx)
