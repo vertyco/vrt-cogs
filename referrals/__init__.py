b59d33fbf3e57d97fd47b5e9e8cd6cc9994cd4e6
Vert <alex.c.goble@gmail.com>
2025-01-03 23:18:43 -0500

    Referrals - include context menu

--- referrals/__init__.py
+++ referrals/__init__.py
@@ -1,6 +1,7 @@
 from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 
+from .commands.user import referredby_context
 from .main import Referrals
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
@@ -8,3 +9,8 @@ __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot: Red):
     await bot.add_cog(Referrals(bot))
+    bot.tree.add_command(referredby_context)
+
+
+async def teardown(bot: Red):
+    bot.tree.remove_command(referredby_context)
