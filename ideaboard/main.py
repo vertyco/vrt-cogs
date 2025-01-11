ed874b993f7dccbb336f56905653eab02c0f8e8a
Vert <alex.c.goble@gmail.com>
2025-01-10 19:05:05 -0500

    IdeaBoard - dont block blacklisted users from voting, just making suggestions

--- ideaboard/main.py
+++ ideaboard/main.py
@@ -20,7 +20,7 @@ class IdeaBoard(Commands, Listeners, commands.Cog, metaclass=CompositeMetaClass)
     """Share Ideas and Suggestions"""
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "0.7.12"
+    __version__ = "0.7.13"
 
     def __init__(self, bot: Red):
         super().__init__()
