9b25398453533c318b80e5e0971b04377c95274f
Vert <alex.c.goble@gmail.com>
2025-02-01 18:58:54 -0500

    Appeals - only send response for help if command is invoked

--- appeals/common/checks.py
+++ appeals/common/checks.py
@@ -4,8 +4,6 @@ import discord
 from discord.ext.commands.core import check
 from redbot.core import commands
 
-from ..db.tables import AppealGuild
-
 
 def ensure_db_connection():
     """Decorator to ensure a database connection is active.
@@ -22,21 +20,6 @@ def ensure_db_connection():
     async def predicate(ctx: commands.Context) -> bool:
         if not ctx.cog.db:
             txt = "Database connection is not active, try again later"
-            with suppress(discord.HTTPException):
-                await ctx.send(txt, ephemeral=True)
-            return False
-        return True
-
-    return check(predicate)
-
-
-def ensure_appeal_system_ready():
-    async def predicate(ctx: commands.Context) -> bool:
-        txt = (
-            "This server hasn't been set up for the appeal system yet!\n"
-            f"Type `{ctx.clean_prefix}appeal help` to get started."
-        )
-        if not await AppealGuild.exists().where(AppealGuild.id == ctx.guild.id):
             with suppress(discord.HTTPException):
                 await ctx.send(txt, ephemeral=True)
             return False
