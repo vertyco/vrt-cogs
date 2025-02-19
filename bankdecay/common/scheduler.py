c21a1a9fef055c4a904a55953da8362c574c969b
Vert <alex.c.goble@gmail.com>
2025-02-19 09:28:18 -0500

    BankDecay - handle when system timezone isnt utc

--- bankdecay/common/scheduler.py
+++ bankdecay/common/scheduler.py
@@ -1,6 +1,10 @@
+import os
+
 import pytz
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+if "TZ" not in os.environ:
+    os.environ["TZ"] = "UTC"
 scheduler = AsyncIOScheduler(jobstores={"default": MemoryJobStore()})
 scheduler.configure(timezone=pytz.timezone("UTC"))
