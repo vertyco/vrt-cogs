0ab79bd75b7bb79f282950266294a251256460f0
Vert <alex.c.goble@gmail.com>
2025-06-27 18:37:21 -0400

    AssistantUtils - fetch_channel_history update

--- assistantutils/main.py
+++ assistantutils/main.py
@@ -27,7 +27,7 @@ class AssistantUtils(Functions, commands.Cog, metaclass=CompositeMetaClass):
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "0.1.1"
+    __version__ = "0.1.2"
 
     def __init__(self, bot: Red):
         super().__init__()
