1c47c058595699cf8e82dab1360b712489484e80
Vert <alex.c.goble@gmail.com>
2025-07-03 18:36:33 -0400

    Assistant - fix: remove embeddings from collection if not found in config

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -55,7 +55,7 @@ class Assistant(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "6.17.0"
+    __version__ = "6.17.1"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
