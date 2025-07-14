ba1f63dea33df66fbcd7f00a0c4bfcd29de03f08
Vert <alex.c.goble@gmail.com>
2025-07-13 20:14:58 -0400

    Assistant - fix function search so toggle button shows correct color

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -55,7 +55,7 @@ class Assistant(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "6.17.3"
+    __version__ = "6.17.4"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
