314e59248b2ae5b58a43bb021cc81c5b12188a2c
Vert <alex.c.goble@gmail.com>
2025-08-27 19:07:33 -0400

    Taskr - fix missing channel fetch methods

--- taskr/main.py
+++ taskr/main.py
@@ -26,7 +26,7 @@ class Taskr(Commands, commands.Cog, metaclass=CompositeMetaClass):
     """Schedule bot commands with ease"""
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "0.0.17b"
+    __version__ = "0.0.18b"
 
     def __init__(self, bot: Red):
         super().__init__()
