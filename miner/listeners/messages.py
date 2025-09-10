148e2e091de47d3f658a215ec03b07a8c8bf5df0
Vert <alex.c.goble@gmail.com>
2025-09-09 21:59:59 -0400

    Refactor cache clearing for player tools and guild settings to delete specific entries by ID

--- miner/listeners/messages.py
+++ miner/listeners/messages.py
@@ -87,7 +87,7 @@ class MessageListener(MixinMeta):
                 if invalid_users:
                     settings.notify_players = valid_users
                     await settings.save([GuildSettings.notify_players])
-                    await self.db_utils.get_cached_guild_settings.cache.clear()  # type: ignore
+                    await self.db_utils.get_cached_guild_settings.cache.delete(message.guild.id)  # type: ignore
                 if valid_users:
                     mention_str = " ".join(f"<@{i}>" for i in valid_users)
                     await channel.send(f"{mention_str}")
