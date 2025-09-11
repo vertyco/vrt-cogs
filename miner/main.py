15996cf303e9eb891d4c7b64f07863b7ef5ecb69
Vert <alex.c.goble@gmail.com>
2025-09-11 17:10:41 -0400

    Miner - adjust pacing

--- miner/main.py
+++ miner/main.py
@@ -27,7 +27,7 @@ class Miner(Commands, Listeners, TaskLoops, commands.Cog, metaclass=CompositeMet
     """Pickaxe in hand, fortune awaits"""
 
     __author__ = "Vertyco"
-    __version__ = "0.1.24b"
+    __version__ = "0.1.25b"
 
     def __init__(self, bot: Red):
         super().__init__()
