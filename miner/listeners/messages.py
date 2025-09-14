49f89590281db35388961ff3d65afae391249232
Vert <alex.c.goble@gmail.com>
2025-09-14 16:25:35 -0400

    Miner - allow single person to trigger spawn after 10 minutes

--- miner/listeners/messages.py
+++ miner/listeners/messages.py
@@ -34,24 +34,30 @@ class MessageListener(MixinMeta):
         if not self.db_active():
             return
 
-        if message.guild.id not in self.last_user_message:
-            self.last_user_message[message.guild.id] = message.author.id
-        elif self.last_user_message[message.guild.id] == message.author.id:
-            # Same user cannot spam messages to increase activity
-            return
-
-        self.last_user_message[message.guild.id] = message.author.id
-
         settings: GuildSettings = await self.db_utils.get_cached_guild_settings(message.guild.id)
         key = message.channel.id if settings.per_channel_activity_trigger else message.guild.id
         self.activity.update(key)
 
         # Check last rock spawn
         last_spawn = self.last_channel_rock_spawn.get(message.channel.id, 0)
-        time_between_spawns = max(settings.time_between_spawns, constants.MIN_TIME_BETWEEN_SPAWNS)
-        if perf_counter() - last_spawn < time_between_spawns:
+        min_time = max(settings.time_between_spawns, constants.MIN_TIME_BETWEEN_SPAWNS)
+        time_elapsed = perf_counter() - last_spawn
+
+        if time_elapsed < min_time:
             return
-        self.last_channel_rock_spawn[message.channel.id] = perf_counter()
+        else:
+            self.last_channel_rock_spawn[message.channel.id] = perf_counter()
+
+        if message.guild.id not in self.last_user_message:
+            self.last_user_message[message.guild.id] = message.author.id
+        elif (
+            self.last_user_message[message.guild.id] == message.author.id
+            and time_elapsed < constants.ABSOLUTE_MAX_TIME_BETWEEN_SPAWNS
+        ):
+            # Same user cannot spam messages to increase activity
+            return
+        else:
+            self.last_user_message[message.guild.id] = message.author.id
 
         rock_type: constants.RockTierName | None = self.activity.maybe_get_rock(key)
         if not rock_type:
