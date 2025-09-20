add97b2d30d1793e2833a22b27d41a4a1feccb1f
Vert <alex.c.goble@gmail.com>
2025-09-20 11:45:50 -0400

    Profiler - Update track_toggle command to enforce method type and simplify validation

--- profiler/main.py
+++ profiler/main.py
@@ -25,7 +25,7 @@ class Profiler(Owner, Profiling, Wrapper, commands.Cog, metaclass=CompositeMetaC
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "1.5.1"
+    __version__ = "1.5.2"
 
     def __init__(self, bot: Red):
         super().__init__()
