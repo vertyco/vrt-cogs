c9a18e583389974667eb80c5f1c40de77eccf2e6
Vert <alex.c.goble@gmail.com>
2025-09-27 21:38:34 -0400

    Metrics - improve EconomyTrack import to use async iter

--- metrics/main.py
+++ metrics/main.py
@@ -25,7 +25,7 @@ class Metrics(Commands, TaskLoops, commands.Cog, metaclass=CompositeMetaClass):
     """Track various metrics about your server."""
 
     __author__ = "Vertyco"
-    __version__ = "0.0.2b"
+    __version__ = "0.0.3b"
 
     def __init__(self, bot: Red):
         super().__init__()
