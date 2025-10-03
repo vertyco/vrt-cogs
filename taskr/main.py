38b8bd8d7af0bca62290928fddc1f70237aba827
Vert <alex.c.goble@gmail.com>
2025-10-03 09:13:49 -0400

    Taskr - fix AttributeError

--- taskr/main.py
+++ taskr/main.py
@@ -26,7 +26,7 @@ class Taskr(Commands, commands.Cog, metaclass=CompositeMetaClass):
     """Schedule bot commands with ease"""
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "0.1.4b"
+    __version__ = "0.1.5b"
 
     def __init__(self, bot: Red):
         super().__init__()
