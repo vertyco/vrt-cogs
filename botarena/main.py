c633ed3ce3e519982b49571edcd0f9f83078dea8
Vert <alex.c.goble@gmail.com>
2026-01-23 10:15:08 -0500

    BotArena - menu breaking after selling a bot

--- botarena/main.py
+++ botarena/main.py
@@ -27,7 +27,7 @@ from .constants import CHASSIS, COMPONENTS, PLATING
 log = logging.getLogger("red.vrt.botarena")
 
 __author__ = "Vertyco"
-__version__ = "1.2.3a"
+__version__ = "1.2.4a"
 
 
 class BotArena(Commands, commands.Cog, metaclass=CompositeMetaClass):
