631568837ad92c6dbc3744efe6c14d58fd13dd62
Vert <alex.c.goble@gmail.com>
2026-01-25 12:57:51 -0500

    Tickets - Fix log message deletion by ensuring log channel exists before fetching message; update version to 2.14.2

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -36,7 +36,7 @@ class Tickets(TicketCommands, Functions, commands.Cog, metaclass=CompositeMetaCl
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "2.14.1"
+    __version__ = "2.14.2"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
