560ca5ce4041e0eb69c616257f2c4f65f45868fb
Vert <alex.c.goble@gmail.com>
2026-02-15 00:15:37 -0500

    Tickets - add `escalate` command to escalate a ticket to admins only

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -37,7 +37,7 @@ class Tickets(TicketCommands, Functions, commands.Cog, metaclass=CompositeMetaCl
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "3.0.5"
+    __version__ = "3.1.0"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
