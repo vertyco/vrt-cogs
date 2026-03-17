0cdb12a6b58ccd50012f2564274feb9c0b53eb91
Vert <alex.c.goble@gmail.com>
2026-03-17 14:39:49 -0400

    Assistant - update new supported models

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -68,7 +68,7 @@ class Assistant(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "7.5.2"
+    __version__ = "7.5.3"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
