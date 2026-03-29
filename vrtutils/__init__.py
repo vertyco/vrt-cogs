800df0de3c2b64d8a89f5fe117a803d2bdd60b5c
Jakub Kuczys <me@jacken.men>
2026-03-29 23:59:49 +0200

    Update quickpull command to stop reimplementing Downloader (+ 3.5.25 support)

--- vrtutils/__init__.py
+++ vrtutils/__init__.py
@@ -2,6 +2,7 @@ from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 
 from .commands.todo import mock_edit_message
+from .commands.updates import monkeypatch_repo, revert_monkeypatch_repo
 from .vrtutils import VrtUtils
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
@@ -10,7 +11,9 @@ __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 async def setup(bot: Red):
     await bot.add_cog(VrtUtils(bot))
     bot.tree.add_command(mock_edit_message)
+    monkeypatch_repo()
 
 
 async def teardown(bot: Red):
     bot.tree.remove_command(mock_edit_message)
+    revert_monkeypatch_repo()
