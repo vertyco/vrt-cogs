a9335e348248f01c4bf86c66947705adbcd59af2
Vert <alex.c.goble@gmail.com>
2026-06-10 23:05:55 -0400

    Assistant - combine convostats and convocontext

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -73,7 +73,7 @@ class Assistant(
     """
 
     __author__ = "[vertyco](https://github.com/vertyco/vrt-cogs)"
-    __version__ = "8.14.0"
+    __version__ = "8.15.0"
 
     def format_help_for_context(self, ctx):
         helpcmd = super().format_help_for_context(ctx)
