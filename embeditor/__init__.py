8509585b92ab563b1578233500cfc5361e04f3ea
Vert <alex.c.goble@gmail.com>
2026-07-02 10:04:20 -0400

    Embeditor - fix import issue

--- embeditor/__init__.py
+++ embeditor/__init__.py
@@ -1,13 +1,13 @@
 from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 
-from .main import EmbEditor, full_edit_message, quick_edit_message
+from .main import Embeditor, full_edit_message, quick_edit_message
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 
 async def setup(bot: Red):
-    await bot.add_cog(EmbEditor(bot))
+    await bot.add_cog(Embeditor(bot))
     bot.tree.add_command(quick_edit_message)
     bot.tree.add_command(full_edit_message)
 
