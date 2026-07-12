6c62a4ed8554eb5c3799eb6ea0d47a6c1a5cb9ea
Vert <alex.c.goble@gmail.com>
2026-07-12 11:00:52 -0400

    Miner - make all commands guild only

--- miner/main.py
+++ miner/main.py
@@ -35,7 +35,7 @@ class Miner(Commands, Listeners, TaskLoops, commands.Cog, metaclass=CompositeMet
     """Pickaxe in hand, fortune awaits"""
 
     __author__ = "Vertyco"
-    __version__ = "1.2.1"
+    __version__ = "1.2.2"
 
     def __init__(self, bot: Red):
         super().__init__()
